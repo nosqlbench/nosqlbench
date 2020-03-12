5108e41ef55943d88e6b5a49a8672d3fcefa1778
Jonathan Shook <jshook@gmail.com>
2020-03-12 18:23:00 -0500

    actions debugging

--- scripts/avoid-release-loop.sh
+++ scripts/avoid-release-loop.sh
@@ -6,7 +6,8 @@ GIT_RELEASE_BOT_NAME=${GIT_RELEASE_BOT_NAME:?GIT_RELEASE_BOT_NAME must be provid
 GITHUB_SHA=${GITHUB_SHA:?GITHUB_SHA must be provided}
 
 # avoid the release loop by checking if the latest commit is a release commit
-readonly local last_release_commit_hash=$(git log --pretty=format:"%H" -1)
+readonly local last_release_commit_hash=$(git log --grep='\[maven-release-plugin\] prepare release' --pretty=format:"%H" -1)
+
 echo "Last $GIT_RELEASE_BOT_NAME commit: ${last_release_commit_hash}"
 echo "Current commit: ${GITHUB_SHA}"
 if [[ "${last_release_commit_hash}" = "${GITHUB_SHA}" ]]; then
