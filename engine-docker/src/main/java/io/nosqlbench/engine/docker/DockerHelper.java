250a027647c53ba0d42adf6bc96948e8e16a0923
phact <estevezsebastian@gmail.com>
2020-03-26 18:12:58 -0400

    refactor first

---
+++ engine-docker/src/main/java/io/nosqlbench/engine/docker/DockerHelper.java
@@ -0,0 +1,204 @@
+package io.nosqlbench.engine.docker;
+
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.DockerCmdExecFactory;
+import com.github.dockerjava.api.command.ListContainersCmd;
+import com.github.dockerjava.api.command.LogContainerCmd;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.async.ResultCallbackTemplate;
+import com.github.dockerjava.core.command.LogContainerResultCallback;
+import com.github.dockerjava.core.command.PullImageResultCallback;
+import com.github.dockerjava.okhttp.OkHttpDockerCmdExecFactory;
+import com.sun.security.auth.module.UnixSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static io.nosqlbench.engine.docker.RestHelper.post;
+
+public class DockerHelper {
+    private static final String DOCKER_HOST = "DOCKER_HOST";
+    private static final String DOCKER_HOST_ADDR = "unix:///var/run/docker.sock";
+//    private Client rsClient = ClientBuilder.newClient();
+
+    private DockerClientConfig config;
+    private DockerClient dockerClient;
+    private Logger logger = LoggerFactory.getLogger(DockerHelper.class);
+
+    public DockerHelper(){
+        System.getProperties().setProperty(DOCKER_HOST, DOCKER_HOST_ADDR);
+        this.config = DefaultDockerClientConfig.createDefaultConfigBuilder().withDockerHost(DOCKER_HOST_ADDR).build();
+        DockerCmdExecFactory dockerCmdExecFactory = new OkHttpDockerCmdExecFactory()
+            .withReadTimeout(60000)
+            .withConnectTimeout(60000);
+
+        this.dockerClient = DockerClientBuilder.getInstance(config)
+            .withDockerCmdExecFactory(dockerCmdExecFactory)
+            .build();
+    }
+    public String startDocker(String IMG, String tag, String name, List<Integer> ports, List<String> volumeDescList, List<String> envList, List<String> cmdList, String reload) {
+        logger.debug("Starting docker with img=" + IMG + ", tag=" + tag + ", name=" + name + ", " +
+            "ports=" + ports + ", volumes=" + volumeDescList + ", env=" + envList + ", cmds=" + cmdList + ", reload=" + reload);
+        ListContainersCmd listContainersCmd = dockerClient.listContainersCmd().withStatusFilter(List.of("exited"));
+        listContainersCmd.getFilters().put("name", Arrays.asList(name));
+        List<Container> stoppedContainers = null;
+        try {
+            stoppedContainers = listContainersCmd.exec();
+            for (Container stoppedContainer : stoppedContainers) {
+                String id = stoppedContainer.getId();
+                logger.info("Removing exited container: " + id);
+                dockerClient.removeContainerCmd(id).exec();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Unable to contact docker, make sure docker is up and try again.");
+            logger.error("If docker is installed make sure this user has access to the docker group.");
+            logger.error("$ sudo gpasswd -a ${USER} docker && newgrp docker");
+            System.exit(1);
+        }
+
+        Container containerId = searchContainer(name, reload);
+        if (containerId != null) {
+            return containerId.getId();
+        }
+
+        Info info = dockerClient.infoCmd().exec();
+        dockerClient.buildImageCmd();
+
+        String term = IMG.split("/")[1];
+        //List<SearchItem> dockerSearch = dockerClient.searchImagesCmd(term).exec();
+        List<Image> dockerList = dockerClient.listImagesCmd().withImageNameFilter(IMG).exec();
+        if (dockerList.size() == 0) {
+            dockerClient.pullImageCmd(IMG)
+                .withTag(tag)
+                .exec(new PullImageResultCallback()).awaitSuccess();
+
+            dockerList = dockerClient.listImagesCmd().withImageNameFilter(IMG).exec();
+            if (dockerList.size() == 0) {
+                logger.error(String.format("Image %s not found, unable to automatically pull image." +
+                        " Check `docker images`",
+                    IMG));
+                System.exit(1);
+            }
+        }
+        logger.info("Search returned" + dockerList.toString());
+
+
+        List<ExposedPort> tcpPorts = new ArrayList<>();
+        List<PortBinding> portBindings = new ArrayList<>();
+        for (Integer port : ports) {
+            ExposedPort tcpPort = ExposedPort.tcp(port);
+            Ports.Binding binding = new Ports.Binding("0.0.0.0", String.valueOf(port));
+            PortBinding pb = new PortBinding(binding, tcpPort);
+
+            tcpPorts.add(tcpPort);
+            portBindings.add(pb);
+        }
+
+        List<Volume> volumeList = new ArrayList<>();
+        List<Bind> volumeBindList = new ArrayList<>();
+        for (String volumeDesc : volumeDescList) {
+            String volFrom = volumeDesc.split(":")[0];
+            String volTo = volumeDesc.split(":")[1];
+            Volume vol = new Volume(volTo);
+            volumeList.add(vol);
+            volumeBindList.add(new Bind(volFrom, vol));
+        }
+
+
+        CreateContainerResponse containerResponse;
+        if (envList == null) {
+            containerResponse = dockerClient.createContainerCmd(IMG + ":" + tag)
+                .withCmd(cmdList)
+                .withExposedPorts(tcpPorts)
+                .withHostConfig(
+                    new HostConfig()
+                        .withPortBindings(portBindings)
+                        .withPublishAllPorts(true)
+                        .withBinds(volumeBindList)
+                )
+                .withName(name)
+                //.withVolumes(volumeList)
+                .exec();
+        } else {
+            long user = new UnixSystem().getUid();
+            containerResponse = dockerClient.createContainerCmd(IMG + ":" + tag)
+                .withEnv(envList)
+                .withExposedPorts(tcpPorts)
+                .withHostConfig(
+                    new HostConfig()
+                        .withPortBindings(portBindings)
+                        .withPublishAllPorts(true)
+                        .withBinds(volumeBindList)
+                )
+                .withName(name)
+                .withUser(""+user)
+                //.withVolumes(volumeList)
+                .exec();
+        }
+
+        dockerClient.startContainerCmd(containerResponse.getId()).exec();
+
+        return containerResponse.getId();
+
+    }
+
+    public Container searchContainer(String name, String reload) {
+
+        ListContainersCmd listContainersCmd = dockerClient.listContainersCmd().withStatusFilter(List.of("running"));
+        listContainersCmd.getFilters().put("name", Arrays.asList(name));
+        List<Container> runningContainers = null;
+        try {
+            runningContainers = listContainersCmd.exec();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Unable to contact docker, make sure docker is up and try again.");
+            System.exit(1);
+        }
+
+        if (runningContainers.size() >= 1) {
+            //Container test = runningContainers.get(0);
+            logger.info(String.format("The container %s is already running", name));
+
+            logger.info(String.format("Hupping config"));
+
+            if (reload != null) {
+                post(reload, null, false, "reloading config");
+            }
+
+            return runningContainers.get(0);
+        }
+        return null;
+    }
+
+    public void pollLog(String containerId, ResultCallbackTemplate<LogContainerResultCallback, Frame> logCallback) {
+
+        LogContainerResultCallback loggingCallback = new
+            LogContainerResultCallback();
+
+        LogContainerCmd cmd = dockerClient.logContainerCmd(containerId)
+            .withStdOut(true)
+            .withFollowStream(true)
+            .withTailAll();
+
+        final boolean[] httpStarted = {false};
+        cmd.exec(logCallback);
+
+        try {
+            loggingCallback.awaitCompletion(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            logger.error("Error getting docker log and detect start for containerId: " + containerId);
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ engine-docker/src/main/java/io/nosqlbench/engine/docker/RestHelper.java
@@ -0,0 +1,77 @@
+package io.nosqlbench.engine.docker;
+
+import io.nosqlbench.engine.api.exceptions.BasicError;
+import io.nosqlbench.engine.api.util.NosqlBenchFiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
+
+public class RestHelper {
+    private static Logger logger = LoggerFactory.getLogger(RestHelper.class);
+
+    static HttpClient.Builder clientBuilder = HttpClient.newBuilder();
+    static HttpClient httpClient = clientBuilder.build();
+
+
+    private static String basicAuth(String username, String password) {
+        return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+    }
+
+
+    public static HttpResponse<String> post(String url, String path, boolean auth, String taskname) {
+        logger.debug("posting to " + url + " with path:" + path +", auth: " + auth + " task:" + taskname);
+
+        HttpRequest.Builder builder = HttpRequest.newBuilder();
+        builder = builder.uri(URI.create(url));
+        if (auth) {
+            // do not, DO NOT put authentication here that is not a well-known default already
+            // DO prompt the user to configure a new password on first authentication
+            builder = builder.header("Authorization", basicAuth("admin", "admin"));
+        }
+
+        if (path !=null) {
+            logger.debug("POSTing " + path + " to " + url);
+            String dashboard = NosqlBenchFiles.readFile(path);
+            logger.debug("length of content for " + path + " is " + dashboard.length());
+            builder = builder.POST(HttpRequest.BodyPublishers.ofString(dashboard));
+            builder.setHeader("Content-Type", "application/json");
+        } else {
+            logger.debug(("POSTing empty body to " + url));
+            builder = builder.POST(HttpRequest.BodyPublishers.noBody());
+        }
+
+        HttpRequest request = builder.build();
+
+        try {
+            HttpResponse<String> resp = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.debug("http response for configuring grafana:\n" + resp);
+            logger.debug("response status code: " + resp.statusCode());
+            logger.debug("response body: " + resp.body());
+            if (resp.statusCode()==412) {
+                logger.warn("Unable to configure dashboard, grafana precondition failed (status 412): " + resp.body());
+                String err = "When trying to configure grafana, any errors indicate that you may be trying to RE-configure an instance." +
+                    " This may be a bug. If you already have a docker stack running, you can just use '--report-graphite-to localhost:9109'\n" +
+                    " instead of --docker-metrics.";
+                throw new BasicError(err);
+            } else if (resp.statusCode()==401 && resp.body().contains("Invalid username")) {
+                logger.warn("Unable to configure dashboard, grafana authentication failed (status " + resp.statusCode() + "): " + resp.body());
+                String err = "Grafana does not have the same password as expected for a new container. We shouldn't be trying to add dashboards on an" +
+                    " existing container. This may be a bug. If you already have a docker stack running, you can just use '--report-graphite-to localhost:9109'" +
+                    " instead of --docker-metrics.";
+                throw new BasicError(err);
+            } else if (resp.statusCode()<200 || resp.statusCode()>200) {
+                logger.error("while trying to " + taskname +", received status code " + resp.statusCode() + " while trying to auto-configure grafana, with body:");
+                logger.error(resp.body());
+                throw new RuntimeException("while trying to " + taskname + ", received status code " + resp.statusCode() + " response for " + url + " with body: " + resp.body());
+            }
+            return resp;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
