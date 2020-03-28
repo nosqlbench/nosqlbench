7a754084236356f744983b937a40affe12314388
Jonathan Shook <jshook@gmail.com>
2020-03-28 01:51:48 -0500

    refactor the virtdata docs facility

---
+++ virtdata-userlibs/src/test/java/io/nosqlbench/virtdata/userlibs/apps/docsapp/FDocFuncsTest.java
@@ -0,0 +1,25 @@
+package io.nosqlbench.virtdata.userlibs.apps.docsapp;
+
+import io.nosqlbench.virtdata.processors.ExampleDocData;
+import io.nosqlbench.virtdata.userlibs.apps.docsapp.fdocs.ExampleDocFunc1;
+import io.nosqlbench.virtdata.userlibs.apps.docsapp.fdocs.ExampleDocFunc2;
+import io.nosqlbench.virtdata.userlibs.apps.docsapp.fdocs.FDocFunc;
+import io.nosqlbench.virtdata.userlibs.apps.docsapp.fdocs.FDocFuncs;
+import org.testng.annotations.Test;
+
+public class FDocFuncsTest {
+
+    @Test
+    public void testMarkdownFormat() {
+        ExampleDocFunc1 exampleDocData1 = new ExampleDocFunc1();
+        ExampleDocFunc2 exampleDocData2 = new ExampleDocFunc2();
+        FDocFuncs funcs = new FDocFuncs(exampleDocData1.getClassName());
+
+        funcs.addFunctionDoc(new FDocFunc(exampleDocData1));
+        funcs.addFunctionDoc(new FDocFunc(exampleDocData2));
+
+        String out = funcs.asMarkdown();
+        System.out.print(out);
+    }
+
+}
