580eac4b09becadeaa3dee188b55ba11042e2871
Jonathan Shook <jshook@gmail.com>
2020-04-13 15:38:18 -0500

    added escaping support to Template

--- virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/shared/unary_string/TemplateTest.java
+++ virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/shared/unary_string/TemplateTest.java
@@ -52,4 +52,16 @@ public class TemplateTest {
         }
     }
 
+    @Test
+    public void testUnescaping() {
+        String escaped="front \\' back";
+        LongFunction<String> func = String::valueOf;
+        Template template = new Template("{} extra", func);
+        String unescaped = template.unescape(escaped);
+        assertThat(unescaped).isEqualTo("front ' back");
+
+        String unescaped2= template.unescape("\\' one \\' two \\'");
+        assertThat(unescaped2).isEqualTo("' one ' two '");
+    }
+
 }
