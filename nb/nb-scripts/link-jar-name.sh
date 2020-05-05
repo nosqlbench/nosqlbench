014f98e54e5214198d692f2128f71c182b981699
Jonathan Shook <jshook@gmail.com>
2020-05-05 01:09:16 -0500

    build with assembly instead of shade

--- nb/nb-scripts/link-jar-name.sh
+++ nb/nb-scripts/link-jar-name.sh
@@ -4,20 +4,31 @@ set -x
 
 NBJAR_VERSION=${NBJAR_VERSION:?NBJAR_VERSION must be specified}
 echo "NBJAR_VERSION: ${NBJAR_VERSION}"
-JARNAME="nb-${NBJAR_VERSION}.jar"
+
+
+
 cd target
 if [ -e "nb.jar" ]
 then
  echo "nb.jar link exists, skipping"
  exit 0
 fi
-if [ ! -e "$JARNAME" ]
-then
- echo "$JARNAME does not exist, skipping"
- exit 0
-fi
 
-echo "linking $JARNAME to nb.jar"
-ln -s $JARNAME nb.jar
-echo "linked $JARNAME to nb.jar, exiting"
+for qualifier in jar-with-dependencies
+do
+  JARNAME="nb-${NBJAR_VERSION}-${qualifier}.jar"
+  if [ -e "$JARNAME" ]
+  then
+     echo "linking $JARNAME to nb.jar"
+     ln -s $JARNAME nb.jar
+     echo "linked $JARNAME to nb.jar, exiting"
+     exit 0
+   else
+     echo "$JARNAME does not exist, skipping"
+  fi
+done
+
+
+echo "Unable to find any jar to link to nb.jar name.."
+exit 2
 
