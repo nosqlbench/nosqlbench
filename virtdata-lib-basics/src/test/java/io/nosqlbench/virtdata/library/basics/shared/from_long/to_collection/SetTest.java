17a9c1abbb9dd4ecd7547e88e6e0dd1035bc39c7
Jonathan Shook <jshook@gmail.com>
2020-06-18 11:08:37 -0500

    fix test signature

--- virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_collection/SetTest.java
+++ virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_collection/SetTest.java
@@ -11,14 +11,14 @@ public class SetTest {
 
     @Test
     public void testSet() {
-        Set set = new Set((LongToIntFunction) s -> 3, (LongFunction) e -> e);
+        Set set = new Set((LongToIntFunction) s -> 3, (LongFunction<Object>) e -> e);
         java.util.Set<Object> s1 = set.apply(15L);
         assertThat(s1).containsOnly(15L,16L,17L);
     }
 
     @Test
     public void testStringSet() {
-        StringSet set = new StringSet(s -> 3, (LongToIntFunction) (e -> (int)e));
+        StringSet set = new StringSet((LongToIntFunction) s -> 3, (LongToIntFunction) (e -> (int)e));
         java.util.Set<String> s1 = set.apply(15L);
         assertThat(s1).containsOnly("15","16","17");
     }
