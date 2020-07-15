1cc9d209d00d16a840d702cf35898f4b2c97a0cd
Jonathan Shook <jshook@gmail.com>
2020-07-15 12:33:13 -0500

    Add SSL context init to JMX driver

--- driver-jmx/src/main/java/io/nosqlbench/driver/jmx/JMXActivity.java
+++ driver-jmx/src/main/java/io/nosqlbench/driver/jmx/JMXActivity.java
@@ -4,10 +4,14 @@ import io.nosqlbench.engine.api.activityapi.core.Activity;
 import io.nosqlbench.engine.api.activityapi.planning.OpSequence;
 import io.nosqlbench.engine.api.activityimpl.ActivityDef;
 import io.nosqlbench.engine.api.activityimpl.SimpleActivity;
+import io.nosqlbench.engine.api.util.SSLKsFactory;
+
+import javax.net.ssl.SSLContext;
 
 public class JMXActivity extends SimpleActivity implements Activity {
 
     private OpSequence<ReadyJmxOp> sequence;
+    private SSLContext sslContext;
 
     public JMXActivity(ActivityDef activityDef) {
         super(activityDef);
@@ -18,7 +22,17 @@ public class JMXActivity extends SimpleActivity implements Activity {
         super.initActivity();
         this.sequence = createOpSequenceFromCommands(ReadyJmxOp::new);
         setDefaultsFromOpSequence(sequence);
+        this.sslContext= SSLKsFactory.get().getContext(activityDef);
+
         // TODO: Require qualified default with an op sequence as the input
+    }
+
+    /**
+     * If this is null, then no SSL is requested.
+     * @return The SSLContext for this activity
+     */
+    public SSLContext getSslContext() {
+        return sslContext;
     }
 
     public OpSequence<ReadyJmxOp> getSequencer() {
