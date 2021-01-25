cd86ea756d05e3b0be9f073f0681782ea1e4565a
Jonathan Shook <jshook@gmail.com>
2021-01-25 14:25:46 -0600

    improve error handling

--- driver-http/src/main/java/io/nosqlbench/activitytype/http/InvalidStatusCodeException.java
+++ driver-http/src/main/java/io/nosqlbench/activitytype/http/InvalidStatusCodeException.java
@@ -7,6 +7,11 @@ public class InvalidStatusCodeException extends RuntimeException {
     private final Pattern ok_status;
     private final int statusCode;
 
+    @Override
+    public String getMessage() {
+        return "Server returned status code '" + statusCode + "' which did not match ok_status '" + ok_status.toString() + "'";
+    }
+
     public InvalidStatusCodeException(long cycleValue, Pattern ok_status, int statusCode) {
         this.cycleValue = cycleValue;
         this.ok_status = ok_status;
