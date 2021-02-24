c3910d69a470fb9acb69408c18097b96b312ec36
Jonathan Shook <jshook@gmail.com>
2021-02-24 13:02:15 -0600

    add hash range scaled with scale factor parameter

--- virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_int/HashRangeScaled.java
+++ virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_int/HashRangeScaled.java
@@ -9,11 +9,23 @@ import java.util.function.LongToIntFunction;
 public class HashRangeScaled implements LongToIntFunction {
 
     private final Hash hash = new Hash();
+    private final double scalefactor;
+
+    public HashRangeScaled(double scalefactor) {
+        this.scalefactor = scalefactor;
+    }
+
+    public HashRangeScaled() {
+        this.scalefactor = 1.0D;
+    }
+
 
     @Override
     public int applyAsInt(long operand) {
-        if (operand==0) { return 0; }
+        if (operand == 0) {
+            return 0;
+        }
         long l = hash.applyAsLong(operand);
-        return (int) ((l % operand) % Integer.MAX_VALUE);
+        return (int) (((l % operand) * scalefactor) % Integer.MAX_VALUE);
     }
 }
--- virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_long/HashRangeScaled.java
+++ virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_long/HashRangeScaled.java
@@ -5,20 +5,29 @@ import io.nosqlbench.virtdata.api.annotations.ThreadSafeMapper;
 import java.util.function.LongUnaryOperator;
 
 /**
- * Return a pseudo-random value which can only be as large as the input.
+ * Return a pseudo-random value which can only be as large as the input times
+ * a scale factor, with a default scale factor of 1.0d
  */
 @ThreadSafeMapper
 public class HashRangeScaled implements LongUnaryOperator {
 
-    private Hash hash = new Hash();
+    private final double scalefactor;
+    private final Hash hash = new Hash();
+
+    public HashRangeScaled(double scalefactor) {
+        this.scalefactor = scalefactor;
+    }
 
     public HashRangeScaled() {
+        this.scalefactor = 1.0D;
     }
 
     @Override
     public long applyAsLong(long operand) {
-        if (operand==0) { return 0; }
+        if (operand == 0) {
+            return 0;
+        }
         long hashed = hash.applyAsLong(operand);
-        return hashed % operand;
+        return (long) ((hashed % operand) * scalefactor);
     }
 }
