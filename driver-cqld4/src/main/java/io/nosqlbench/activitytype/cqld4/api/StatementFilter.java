a27ea6cdb2d2daa5a8ba1c13e05d1cbb74285cbf
Jonathan Shook <jshook@gmail.com>
2021-02-25 13:14:26 -0600

    reset cqld4 for reinit

--- driver-cqld4/src/main/java/io/nosqlbench/activitytype/cqld4/api/StatementFilter.java
+++
@@ -1,7 +0,0 @@
-package io.nosqlbench.activitytype.cqld4.api;
-
-import com.datastax.oss.driver.api.core.cql.Statement;
-
-public interface StatementFilter {
-    boolean matches(Statement<?> statement);
-}
--- driver-cqld4/src/main/java/io/nosqlbench/activitytype/cqld4/api/VerifyApplied.java
+++
@@ -1,7 +0,0 @@
-package io.nosqlbench.activitytype.cqld4.api;
-
-public enum VerifyApplied {
-    ignore,
-    error,
-    retry
-}
