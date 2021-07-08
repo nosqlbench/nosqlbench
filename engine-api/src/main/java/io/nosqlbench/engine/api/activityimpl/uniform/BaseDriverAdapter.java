5dca31efb64da9163b279475d96e0f5d58f646d9
Jonathan Shook <jshook@gmail.com>
2021-07-08 11:49:01 -0500

    BaseDriverAdapter improvements

--- engine-api/src/main/java/io/nosqlbench/engine/api/activityimpl/uniform/BaseDriverAdapter.java
+++ engine-api/src/main/java/io/nosqlbench/engine/api/activityimpl/uniform/BaseDriverAdapter.java
@@ -1,20 +1,40 @@
 package io.nosqlbench.engine.api.activityimpl.uniform;
 
-import java.util.*;
+import io.nosqlbench.engine.api.activityimpl.ActivityDef;
+import io.nosqlbench.engine.api.activityimpl.uniform.fieldmappers.FieldDestructuringMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public abstract class BaseDriverAdapter<R extends Runnable> implements DriverAdapter<R> {
+public abstract class BaseDriverAdapter<R extends Runnable,S>
+    implements DriverAdapter<R,S>, ActivityDefAware {
 
+    private final DriverSpaceCache<? extends S> spaceCache;
+    private ActivityDef activityDef;
+
+    protected BaseDriverAdapter() {
+        this.spaceCache = new DriverSpaceCache<>(getSpaceInitializer());
+    }
+
+    /**
+     * BaseDriverAdapter will take any provided functions from {@link #getOpStmtRemappers()}
+     * and {@link #getOpFieldRemappers()} and construct a preprocessor list. These are applied
+     * successively to the op template fields so long as remapping occurs.
+     * @return a list of preprocessors for op template fields
+     */
     @Override
     public final Function<Map<String, Object>, Map<String, Object>> getPreprocessor() {
         List<Function<Map<String,Object>,Map<String,Object>>> mappers = new ArrayList<>();
         List<Function<Map<String,Object>,Map<String,Object>>> stmtRemappers =
-            getStmtRemappers().stream()
+            getOpStmtRemappers().stream()
                 .map(m -> new FieldDestructuringMapper("stmt",m))
                 .collect(Collectors.toList());
         mappers.addAll(stmtRemappers);
-        mappers.addAll(getFieldRemappers());
+        mappers.addAll(getOpFieldRemappers());
 
         if (mappers.size()==0) {
             return (i) -> i;
@@ -33,54 +53,42 @@ public abstract class BaseDriverAdapter<R extends Runnable> implements DriverAda
     }
 
     /**
+     * <p>Provide a list of field remappers which operate exclusively on the 'stmt' field
+     * in the op template. These are useful, for example, for taking the 'stmt' field
+     * and parsing it into component fields which might otherwise be specified by the user.
+     * This allows users to specify String-form op templates which are automatically
+     * destructured into the canonical field-wise form for a given type of operation.</p>
+     * <br/>
+     * <p>Each function in this list is applied in order. If the function returns a value,
+     * then the 'stmt' field is removed and the resulting map is added to the other
+     * fields in the op template.</p>
      *
+     * @return A list of optionally applied remapping functions.
      */
-    protected final static class FieldDestructuringMapper implements Function<Map<String,Object>,Map<String,Object>> {
-
-        private final String fieldname;
-        private final Function<String, Optional<Map<String, Object>>> thenfunc;
-
-        public FieldDestructuringMapper(String fieldName, Function<String,Optional<Map<String,Object>>> thenfunc) {
-            this.fieldname = fieldName;
-            this.thenfunc = thenfunc;
-        }
-
-        @Override
-        public Map<String, Object> apply(Map<String, Object> stringObjectMap) {
-            if (stringObjectMap.containsKey(fieldname)) {
-                Object o = stringObjectMap.get(fieldname);
-                if (o instanceof CharSequence) {
-                    String rawfield = o.toString();
-                    Optional<Map<String, Object>> optionalResult = thenfunc.apply(rawfield);
-                    if (optionalResult.isPresent()) {
-                        Map<String, Object> resultmap = optionalResult.get();
-                        LinkedHashMap<String, Object> returnmap = new LinkedHashMap<>(stringObjectMap);
-                        returnmap.remove(fieldname);
-                        resultmap.forEach((k,v)->{
-                            if (returnmap.containsKey(k)) {
-                                throw new RuntimeException("element '" + k + "' already exist during field remapping.");
-                            }
-                            returnmap.put(k,v);
-                        });
-                        return returnmap;
-                    } else {
-                        return stringObjectMap;
-                    }
-                } else {
-                    throw new RuntimeException("During op mapping, can't parse something that is not a CharSequence: '" + fieldname + "' (type is " + o.getClass().getCanonicalName() + ")");
-                }
-            } else {
-                return stringObjectMap;
-            }
-        }
-    }
-
-    public List<Function<String, Optional<Map<String,Object>>>> getStmtRemappers() {
+    public List<Function<String, Optional<Map<String,Object>>>> getOpStmtRemappers() {
         return List.of();
     }
 
-    public List<Function<Map<String,Object>,Map<String,Object>>> getFieldRemappers() {
+    /**
+     * <p>Provide a list of field remappers which operate on arbitrary fields.
+     * Each function is applied to the op template fields. </p>
+     * @return
+     */
+    public List<Function<Map<String,Object>,Map<String,Object>>> getOpFieldRemappers() {
         return List.of();
     }
 
+    @Override
+    public DriverSpaceCache<? extends S> getSpaceCache() {
+        return spaceCache;
+    }
+
+    @Override
+    public void setActivityDef(ActivityDef activiytDef) {
+        this.activityDef = activiytDef;
+    }
+
+    public ActivityDef getActivityDef() {
+        return activityDef;
+    }
 }
--- engine-api/src/main/java/io/nosqlbench/engine/api/activityimpl/uniform/DriverAdapter.java
+++ engine-api/src/main/java/io/nosqlbench/engine/api/activityimpl/uniform/DriverAdapter.java
@@ -10,17 +10,24 @@ import java.util.function.Function;
 /**
  * <P>The DriverAdapter interface is expected to be the replacement
  * for the current {@link ActivityType}. This interface takes a simpler
- * approach than the historic NoSQLBench approach. Specifically,
- * all of the core logic which was being pasted into each
+ * approach. Specifically, all of the core logic which was being pasted into each
  * driver type is centralized, and only the necessary interfaces
- * needed for construction new operations are exposed.
+ * needed for construction new operations and shared context are exposed.
+ * This means all drivers can now benefit from cross-cutting enhancements
+ * in the core implementation.
  * </P>
  *
- * This
- *
- * @param <R>
+ * @param <R> The type of Runnable operation which will be used to wrap
+ *           all operations for this driver adapter. This allows you to
+ *           add context or features common to all operations of this
+ *           type.
+ * @param <S> The type of context space used by this driver to hold
+ *           cached instances of clients, session, or other native driver
+ *           esoterica. This is the shared state which might be needed
+ *           during construction of R type operations, or even for individual
+ *           operations.
  */
-public interface DriverAdapter<R extends Runnable> {
+public interface DriverAdapter<R extends Runnable, S> {
 
 
     /**
@@ -34,23 +41,51 @@ public interface DriverAdapter<R extends Runnable> {
      */
     Function<ParsedCommand, OpDispenser<R>> getOpMapper();
 
-    Function<Map<String,Object>,Map<String,Object>> getPreprocessor();
+    /**
+     * The preprocessor function allows the driver adapter to remap
+     * the fields in the op template before they are interpreted canonically.
+     * At this level, the transform is applied once to the input map
+     * (once per op template) to yield the map that is provided to
+     * {@link io.nosqlbench.engine.api.activityimpl.OpMapper} implementations.
+     * @return A function to pre-process the op template fields.
+     */
+    default Function<Map<String,Object>,Map<String,Object>> getPreprocessor() {
+        return f->f;
+    }
 
     /**
-     * The op parsers do additional semantic parsing work on behalf
-     * of the user, according to the rules set by the driver adapter
-     * dev. These rules can, for example, look at a single template
-     * formatted field and break it apart into multiple other fields
-     * which are used to directly drive the construction of an operation.
-     * By doing this, it is possible to mark static text segments
-     * and dynamic insertion points separately, and thus allow the
-     * developer to create optimal construction patterns depending
-     * how much is known. For details on this, see TBD: A section
-     * yet to be written on how to do this the easy way.
-     *
-     * If any of these returns a non-null result, then the map is added
-     * to the fields in the op template, and the stmt field is removed.
-     *
-     * @return optional rewrite rules for op stmt field
+     * When a driver needs to identify an error uniquely for the purposes of
+     * routing it to the correct error handler, or naming it in logs, or naming
+     * metrics, override this method in your activity.
+     * @return A function that can reliably and safely map an instance of Throwable to a stable name.
      */
+    default Function<Throwable,String> getErrorNameMapper() {
+        return t -> t.getClass().getSimpleName();
+    }
+
+    /**
+     * During Adapter Initialization, Op Mapping, Op Synthesis, or
+     * Op Execution, you may need access to some shared context that
+     * could change over time. You can build the type of context
+     * needed and then provide this function to provide new instances
+     * when needed.
+     *
+     * These instance are generally called <em>spaces</em> and are
+     * cached in a {@link DriverSpaceCache<S>}.
+     *
+     * @return A function which can initialize a new S
+     */
+    default Function<String,? extends S> getSpaceInitializer() {
+        return n -> null;
+    }
+
+    /**
+     * The cache of all objects needed within a single instance
+     * of a DriverAdapter which are not operations. These are generally
+     * things needed by operations, or things needed during the
+     * construction of operations.
+     * @return A cache of named objects
+     */
+    DriverSpaceCache<? extends S> getSpaceCache();
+
 }
