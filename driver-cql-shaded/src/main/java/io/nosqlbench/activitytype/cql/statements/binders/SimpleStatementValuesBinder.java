e6c1d206b379941763d30918020bc8ad7a317ac6
Jonathan Shook <jshook@gmail.com>
2021-07-13 16:12:34 -0500

    allow parameterized cql to use standard anchors

--- driver-cql-shaded/src/main/java/io/nosqlbench/activitytype/cql/statements/binders/SimpleStatementValuesBinder.java
+++ driver-cql-shaded/src/main/java/io/nosqlbench/activitytype/cql/statements/binders/SimpleStatementValuesBinder.java
@@ -4,6 +4,13 @@ import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.SimpleStatement;
 import com.datastax.driver.core.Statement;
 import io.nosqlbench.virtdata.core.bindings.ValuesArrayBinder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This binder is not meant to be used with anything but DDL or statements
@@ -14,40 +21,49 @@ import io.nosqlbench.virtdata.core.bindings.ValuesArrayBinder;
 public class SimpleStatementValuesBinder
     implements ValuesArrayBinder<SimpleStatement, Statement> {
 
+    private final static Logger logger = LogManager.getLogger(SimpleStatementValuesBinder.class);
     private final boolean parameterized;
 
     public SimpleStatementValuesBinder(boolean parameterized) {
         this.parameterized = parameterized;
     }
 
+    private final static Pattern anchorPattern = Pattern.compile("(?<name>\\?([a-zA-Z_][-a-zA-Z_.-])*|\\{[a-zA-Z_][-a-zA-Z_.-]*})");
+
     @Override
     public Statement bindValues(SimpleStatement context, Object[] values) {
-        String query = context.getQueryString();
-        if (parameterized) {
-            String[] splits = query.split("\\?");
-            assert splits.length == values.length + 1;
-            StringBuilder sb = new StringBuilder();
-            sb.append(splits[0]);
-            for (int i = 1; i < splits.length; i++) {
-                sb.append(values[i - 1]);
-                sb.append(splits[i]);
-            }
-            query = sb.toString();
-            System.out.println(query);
 
+        String query = context.getQueryString();
+
+        SimpleStatement simpleStatement = null;
+        if (parameterized) {
+            Matcher matcher = anchorPattern.matcher(query);
+            StringBuilder sb = new StringBuilder();
+            List<String> anchors = new ArrayList<>();
+            while (matcher.find()) {
+                String name = matcher.group("name");
+                anchors.add(name == null ? "_unnamed_" : name);
+                matcher.appendReplacement(sb, "?");
+            }
+            matcher.appendTail(sb);
+            assert (anchors.size() == values.length);
+            query = sb.toString();
+            simpleStatement = new SimpleStatement(query, values);
+        } else {
+            simpleStatement = new SimpleStatement(query);
         }
-        SimpleStatement simpleStatement = new SimpleStatement(query);
+
         ConsistencyLevel cl = context.getConsistencyLevel();
-        if(cl != null){
+        if (cl != null) {
             simpleStatement.setConsistencyLevel(context.getConsistencyLevel());
         }
         //Does it really makes senses?
         ConsistencyLevel serial_cl = context.getSerialConsistencyLevel();
-        if(serial_cl != null){
+        if (serial_cl != null) {
             simpleStatement.setSerialConsistencyLevel(context.getSerialConsistencyLevel());
         }
         Boolean idempotent = context.isIdempotent();
-        if(idempotent != null){
+        if (idempotent != null) {
             simpleStatement.setIdempotent(idempotent);
         }
         return simpleStatement;
