b12e2afa66b2f16053ea5ee148eb6a18f0b6c7b2
Jonathan Shook <jshook@gmail.com>
2022-01-05 15:31:59 -0600

    update annotation processors to support J17

--- nb-annotations/src/main/java/io/nosqlbench/nb/annotations/ServiceProcessor.java
+++ nb-annotations/src/main/java/io/nosqlbench/nb/annotations/ServiceProcessor.java
@@ -20,8 +20,7 @@ import java.util.stream.Collectors;
  * <pre>classes/META-INF/services/servicename</pre> file for each
  * implemented and annotated service name.
  */
-@SupportedSourceVersion(SourceVersion.RELEASE_11)
-
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
 public class ServiceProcessor extends AbstractProcessor {
     public final static String SERVICE_NAME = Service.class.getCanonicalName();
 
