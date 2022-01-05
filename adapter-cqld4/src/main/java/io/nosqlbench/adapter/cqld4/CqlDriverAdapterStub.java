0d8f6f7308fd54a792d33976ef3544d2d582635e
Jonathan Shook <jshook@gmail.com>
2022-01-05 15:33:53 -0600

    stub for cql and cqld3 routes to cqld4 with warning

---
+++ adapter-cqld4/src/main/java/io/nosqlbench/adapter/cqld4/CqlDriverAdapterStub.java
@@ -0,0 +1,16 @@
+package io.nosqlbench.adapter.cqld4;
+
+import io.nosqlbench.engine.api.activityimpl.uniform.DriverAdapter;
+import io.nosqlbench.nb.annotations.Service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Service(value = DriverAdapter.class, selector = "cql")
+public class CqlDriverAdapterStub extends Cqld4DriverAdapter {
+    private final static Logger logger = LogManager.getLogger(CqlDriverAdapterStub.class);
+
+    public CqlDriverAdapterStub() {
+        super();
+        logger.warn("This version of NoSQLBench uses the DataStax Java Driver version 4 for all CQL workloads. In this preview version, advanced testing features present in the previous cql and cqld3 drivers are being back-ported. If you need those features, please use only the release artifacts. To suppress this message, use driver=cqld4. This warning will be removed in a future version when all features are completely back-ported.");
+    }
+}
