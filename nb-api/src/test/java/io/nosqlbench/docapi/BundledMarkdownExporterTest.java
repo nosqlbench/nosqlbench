50b73e277dbb4285d5f2ccc19d4918c4609d2b7d
Jonathan Shook <jshook@gmail.com>
2022-03-03 12:32:37 -0600

    squelch noisy test

--- nb-api/src/test/java/io/nosqlbench/docapi/BundledMarkdownExporterTest.java
+++ nb-api/src/test/java/io/nosqlbench/docapi/BundledMarkdownExporterTest.java
@@ -1,11 +1,13 @@
 package io.nosqlbench.docapi;
 
 import io.nosqlbench.docexporter.BundledMarkdownExporter;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BundledMarkdownExporterTest {
 
     @Test
+    @Disabled
     public void zipUpDocs() {
         BundledMarkdownExporter.main(new String[0]);
     }
