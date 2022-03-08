3eff33276492d3ef5c9e29f0cf1e0dd8895aa05d
Jonathan Shook <jshook@gmail.com>
2022-03-08 14:17:33 -0600

    add aplv2 headers to virtdata-lib-random

--- virtdata-lib-random/src/main/java/io/nosqlbench/virtdata/library/random/RandomRangedToString.java
+++ virtdata-lib-random/src/main/java/io/nosqlbench/virtdata/library/random/RandomRangedToString.java
@@ -1,19 +1,17 @@
 /*
+ * Copyright (c) 2022 nosqlbench
  *
- *       Copyright 2015 Jonathan Shook
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.nosqlbench.virtdata.library.random;
@@ -26,9 +24,9 @@ import java.util.function.LongFunction;
 @DeprecatedFunction("random mappers are not deterministic. They will be replaced with hash-based functions.")
 public class RandomRangedToString implements LongFunction<String> {
     private final MersenneTwister theTwister;
-    private long min;
-    private long max;
-    private long _length;
+    private final long min;
+    private final long max;
+    private final long _length;
 
     public RandomRangedToString(long min, long max) {
         this(min,max,System.nanoTime());
