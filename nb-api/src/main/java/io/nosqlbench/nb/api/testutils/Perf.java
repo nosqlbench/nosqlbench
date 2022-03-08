660e937997242addcf1713d58981c9db68beedea
Jonathan Shook <jshook@gmail.com>
2022-03-08 14:15:56 -0600

    add aplv2 headers to nb-api

--- nb-api/src/main/java/io/nosqlbench/nb/api/testutils/Perf.java
+++ nb-api/src/main/java/io/nosqlbench/nb/api/testutils/Perf.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright (c) 2022 nosqlbench
  *
- *    Copyright 2016 jshook
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * /
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.nosqlbench.nb.api.testutils;
@@ -187,8 +186,8 @@ public class Perf implements Iterable<Result> {
     public static class Time implements AutoCloseable {
         private final Perf perf;
         private final long start;
-        private String name;
-        private long ops;
+        private final String name;
+        private final long ops;
 
         public Time(Perf perf, String name, long ops) {
             this.name = name;
