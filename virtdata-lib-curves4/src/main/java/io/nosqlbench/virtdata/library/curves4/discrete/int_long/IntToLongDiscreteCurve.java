fce9223ddd2567588e0289cffb9a97ca9da621e8
Jonathan Shook <jshook@gmail.com>
2022-03-08 14:19:43 -0600

    add aplv2 headers to virtdata-lib-curves4

--- virtdata-lib-curves4/src/main/java/io/nosqlbench/virtdata/library/curves4/discrete/int_long/IntToLongDiscreteCurve.java
+++ virtdata-lib-curves4/src/main/java/io/nosqlbench/virtdata/library/curves4/discrete/int_long/IntToLongDiscreteCurve.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.nosqlbench.virtdata.library.curves4.discrete.int_long;
 
 import io.nosqlbench.virtdata.library.curves4.discrete.common.DiscreteIntLongSampler;
@@ -53,8 +69,8 @@ import java.util.function.IntToLongFunction;
  */
 public class IntToLongDiscreteCurve implements IntToLongFunction {
 
-    private DiscreteDistribution distribution;
-    private IntToLongFunction function;
+    private final DiscreteDistribution distribution;
+    private final IntToLongFunction function;
 
     public final static String COMPUTE="compute";
     public final static String INTERPOLATE="interpolate";
@@ -82,7 +98,7 @@ public class IntToLongDiscreteCurve implements IntToLongFunction {
         }
         for (String s : modslist) {
             if (!validModifiers.contains(s)) {
-                throw new RuntimeException("modifier '" + s + "' is not a valid modifier. Use one of " + validModifiers.toString() + " instead.");
+                throw new RuntimeException("modifier '" + s + "' is not a valid modifier. Use one of " + validModifiers + " instead.");
             }
         }
 
