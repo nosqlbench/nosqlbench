7c82f766b55ef708626b4d39dab05104f87daa85
Jonathan Shook <jshook@gmail.com>
2022-03-08 14:14:08 -0600

    add aplv2 headers to engine-api

--- engine-api/src/main/java/io/nosqlbench/engine/api/activityapi/cyclelog/filters/tristate/CycleResultPredicate.java
+++ engine-api/src/main/java/io/nosqlbench/engine/api/activityapi/cyclelog/filters/tristate/CycleResultPredicate.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright (c) 2022 nosqlbench
  *
- *    Copyright 2016 jshook
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * /
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.nosqlbench.engine.api.activityapi.cyclelog.filters.tristate;
@@ -23,8 +22,8 @@ import java.util.function.Predicate;
 
 public interface CycleResultPredicate extends Predicate<ResultReadable> {
 
-    public static class ResultHasSomeBits implements CycleResultPredicate {
-        private int mask;
+    class ResultHasSomeBits implements CycleResultPredicate {
+        private final int mask;
 
         public ResultHasSomeBits(int mask) {
             this.mask = mask;
@@ -36,8 +35,8 @@ public interface CycleResultPredicate extends Predicate<ResultReadable> {
         }
     }
 
-    public static class ResultHasAllBits implements CycleResultPredicate {
-        private int mask;
+    class ResultHasAllBits implements CycleResultPredicate {
+        private final int mask;
 
         public ResultHasAllBits(int mask) {
             this.mask = mask;
@@ -49,7 +48,7 @@ public interface CycleResultPredicate extends Predicate<ResultReadable> {
         }
     }
 
-    public static class ResultInRange implements CycleResultPredicate {
+    class ResultInRange implements CycleResultPredicate {
 
         private final int min;
         private final int max;
@@ -65,7 +64,7 @@ public interface CycleResultPredicate extends Predicate<ResultReadable> {
         }
     }
 
-    public static class ResultEquals implements CycleResultPredicate {
+    class ResultEquals implements CycleResultPredicate {
         private final int value;
 
         public ResultEquals(int value) {
--- engine-api/src/main/java/io/nosqlbench/engine/api/activityapi/cyclelog/filters/tristate/ResultFilteringSieve.java
+++ engine-api/src/main/java/io/nosqlbench/engine/api/activityapi/cyclelog/filters/tristate/ResultFilteringSieve.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright (c) 2022 nosqlbench
  *
- *    Copyright 2016 jshook
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * /
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.nosqlbench.engine.api.activityapi.cyclelog.filters.tristate;
@@ -25,8 +24,8 @@ import java.util.function.Predicate;
 
 public class ResultFilteringSieve implements TristateFilter<ResultReadable> {
 
-    private Policy defaultPolicy;
-    private List<TristateFilter<ResultReadable>> phases;
+    private final Policy defaultPolicy;
+    private final List<TristateFilter<ResultReadable>> phases;
 
     private ResultFilteringSieve(Policy defaultPolicy, List<TristateFilter<ResultReadable>> phases) {
         this.defaultPolicy = defaultPolicy;
@@ -57,7 +56,7 @@ public class ResultFilteringSieve implements TristateFilter<ResultReadable> {
 
 
     private class InclusiveFilterPredicate implements Predicate<ResultReadable> {
-        private ResultFilteringSieve resultFilteringSieve;
+        private final ResultFilteringSieve resultFilteringSieve;
 
         public InclusiveFilterPredicate(ResultFilteringSieve resultFilteringSieve) {
             this.resultFilteringSieve = resultFilteringSieve;
@@ -88,7 +87,7 @@ public class ResultFilteringSieve implements TristateFilter<ResultReadable> {
     }
 
     public static class Builder {
-        private List<TristateFilter<ResultReadable>> phaseFilters = new ArrayList<>();
+        private final List<TristateFilter<ResultReadable>> phaseFilters = new ArrayList<>();
         private Policy defaultPolicy = Policy.Ignore;
 
         public Builder keepByDefault() {
