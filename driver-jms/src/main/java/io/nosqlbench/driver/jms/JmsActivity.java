818cb914cf17a42b83b813b03f960d5afc3999b7
Jonathan Shook <jshook@gmail.com>
2022-05-18 12:26:32 -0500

    update jms for API changes

--- driver-jms/src/main/java/io/nosqlbench/driver/jms/JmsActivity.java
+++ driver-jms/src/main/java/io/nosqlbench/driver/jms/JmsActivity.java
@@ -35,7 +35,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import javax.jms.Destination;
 import javax.jms.JMSContext;
-import javax.jms.JMSException;
 import java.util.concurrent.ConcurrentHashMap;
 
 
@@ -106,13 +105,8 @@ public class JmsActivity extends SimpleActivity {
         }
 
         PulsarConnectionFactory factory;
-        try {
-            factory = new PulsarConnectionFactory(jmsConnInfo.getJmsConnConfig());
-            this.jmsContext = factory.createContext();
-        } catch (JMSException e) {
-            throw new RuntimeException(
-                "Unable to initialize JMS connection factory (driver type: " + jmsProviderType + ")!");
-        }
+        factory = new PulsarConnectionFactory(jmsConnInfo.getJmsConnConfig());
+        this.jmsContext = factory.createContext();
 
         bindTimer = ActivityMetrics.timer(activityDef, "bind");
         executeTimer = ActivityMetrics.timer(activityDef, "execute");
