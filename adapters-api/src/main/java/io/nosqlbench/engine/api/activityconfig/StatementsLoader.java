7a3c3c583fbd714bf24d176377344d4e48827975
Jonathan Shook <jshook@gmail.com>
2022-06-23 00:13:11 -0500

    fix broken map mutation

--- adapters-api/src/main/java/io/nosqlbench/engine/api/activityconfig/StatementsLoader.java
+++ adapters-api/src/main/java/io/nosqlbench/engine/api/activityconfig/StatementsLoader.java
@@ -43,7 +43,9 @@ public class StatementsLoader {
         StmtsDocList layered = new StmtsDocList(rawDocList);
         transformer.checkpointAccesses().forEach((k,v) -> {
             layered.addTemplateVariable(k,v);
-            params.remove(k);
+            if (params.containsKey(k)) {
+                 params.remove(k);
+            }
         });
         return layered;
     }
@@ -59,7 +61,9 @@ public class StatementsLoader {
         StmtsDocList layered = new StmtsDocList(rawStmtsDocList);
         transformer.checkpointAccesses().forEach((k,v) -> {
             layered.addTemplateVariable(k,v);
-            params.remove(k);
+            if (params.containsKey(k)) {
+                 params.remove(k);
+            }
         });
         return layered;
     }
