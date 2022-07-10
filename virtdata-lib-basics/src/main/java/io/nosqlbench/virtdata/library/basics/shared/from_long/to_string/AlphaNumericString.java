de0dd357e53c4cd594aada94c5bbd67e43b1b693
yabinmeng <yabinmeng@gmail.com>
2022-07-10 15:04:03 -0500

    Add support for using a sizing function as a parameter to AlphaNumericString()

--- virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_string/AlphaNumericString.java
+++ virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/from_long/to_string/AlphaNumericString.java
@@ -2,13 +2,13 @@ package io.nosqlbench.virtdata.library.basics.shared.from_long.to_string;
 
 /*
  * Copyright (c) 2022 nosqlbench
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,10 +20,13 @@ package io.nosqlbench.virtdata.library.basics.shared.from_long.to_string;
 
 import io.nosqlbench.virtdata.api.annotations.Categories;
 import io.nosqlbench.virtdata.api.annotations.Category;
+import io.nosqlbench.virtdata.api.annotations.Example;
 import io.nosqlbench.virtdata.api.annotations.ThreadSafeMapper;
+import io.nosqlbench.virtdata.api.bindings.VirtDataConversions;
 import io.nosqlbench.virtdata.library.basics.shared.from_long.to_long.Hash;
 
 import java.util.function.LongFunction;
+import java.util.function.LongToIntFunction;
 
 /**
  * Create an alpha-numeric string of the specified length, character-by-character.
@@ -34,20 +37,35 @@ public class AlphaNumericString implements LongFunction<String> {
     private static final String AVAILABLE_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private final ThreadLocal<StringBuilder> threadStringBuilder = ThreadLocal.withInitial(StringBuilder::new);
     private final Hash hash = new Hash();
-    private final int length;
+    private final LongToIntFunction lengthFunc;
 
-    public AlphaNumericString(int length)
-    {
-        if (length < 0)
-        {
-            throw new RuntimeException("AlphaNumericString must have length >= 0");
+
+    @Example({
+        "AlphaNumericString(16)",
+        "Create an alpha numeric String with length of 16"
+    })
+    @Example({
+        "AlphaNumericString(HashRange(10, 14))",
+        "Create an alpha numeric String with length variable limit (10 to 14)"
+    })
+    public AlphaNumericString(int length) {
+        this.lengthFunc = s -> length;
+    }
+
+    public AlphaNumericString(Object lengthFunc) {
+        if (lengthFunc instanceof Number) {
+            int size = ((Number) lengthFunc).intValue();
+            this.lengthFunc = s -> size;
+        } else {
+            this.lengthFunc = VirtDataConversions.adaptFunction(lengthFunc, LongToIntFunction.class);
         }
-        this.length = length;
     }
 
     @Override
     public String apply(long operand)
     {
+        int length = lengthFunc.applyAsInt(operand);
+
         long hashValue = operand;
         StringBuilder sb = threadStringBuilder.get();
         sb.setLength(0);
@@ -63,6 +81,6 @@ public class AlphaNumericString implements LongFunction<String> {
     @Override
     public String toString()
     {
-        return "AlphaNumericString(length=" + length + ")";
+        return "AlphaNumericString(length=" + lengthFunc.applyAsInt(0) + ")";
     }
 }
--- virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/tests/long_string/AlphaNumericStringTest.java
+++ virtdata-lib-basics/src/test/java/io/nosqlbench/virtdata/library/basics/tests/long_string/AlphaNumericStringTest.java
@@ -2,13 +2,13 @@ package io.nosqlbench.virtdata.library.basics.tests.long_string;
 
 /*
  * Copyright (c) 2022 nosqlbench
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,11 +18,15 @@ package io.nosqlbench.virtdata.library.basics.tests.long_string;
  */
 
 
+import io.nosqlbench.virtdata.library.basics.shared.from_long.to_bytebuffer.ByteBufferSizedHashed;
+import io.nosqlbench.virtdata.library.basics.shared.from_long.to_int.HashRange;
 import io.nosqlbench.virtdata.library.basics.shared.from_long.to_string.AlphaNumericString;
 import org.junit.jupiter.api.Test;
 
+import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.LongToIntFunction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -51,6 +55,20 @@ public class AlphaNumericStringTest {
             String value = alphaNumeric.apply(cycle);
             assertThat(value.length()).isEqualTo(20);
             assertThat(seen).contains(value);
+        }
+    }
+
+    @Test
+    public void testAlphaNumericStringWithHashRange() {
+        LongToIntFunction sizeFunc = new HashRange(10, 20);
+        AlphaNumericString alphaNumeric = new AlphaNumericString(sizeFunc);
+        Set<String> seen = new HashSet<>();
+
+        for (long cycle = 0; cycle < 10; cycle++) {
+            String value = alphaNumeric.apply(cycle);
+            assertThat(value.length()).isGreaterThanOrEqualTo(10).isLessThanOrEqualTo(20);
+            assertThat(seen).doesNotContain(value);
+            seen.add(value);
         }
     }
 }
