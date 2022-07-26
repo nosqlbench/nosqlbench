ab03cfc54dc5d1952408e4c7723b5212b2b8fe06
Jonathan Shook <jshook@gmail.com>
2022-07-26 01:15:23 -0500

    start ring analyzer with picocli experiment

---
+++ adapter-cqld4/src/main/java/io/nosqlbench/analysis/RingAnalyzer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.nosqlbench.analysis;
+
+import io.nosqlbench.api.spi.BundledApp;
+import io.nosqlbench.nb.annotations.Service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+
+@Service(value=BundledApp.class, selector = "cql-ring-analyzer")
+public class RingAnalyzer implements BundledApp {
+    private final static Logger logger = LogManager.getLogger(RingAnalyzer.class);
+
+    @Override
+    public int appMain(String[] args) {
+        RingAnalyzerConfig cfg = new RingAnalyzerConfig();
+        CommandLine cli = new CommandLine(cfg);
+        CommandLine.ParseResult cl = cli.parseArgs(args);
+
+        logger.info("filename: " + cfg.filename);
+        return 0;
+    }
+}
