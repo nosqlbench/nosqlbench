30214f857d0b7d5a1803633790c0d9bb210640ee
Jonathan Shook <jshook@gmail.com>
2022-09-09 11:08:41 -0500

    reorder ANSI codes for clarity, fix reset

--- engine-api/src/main/java/io/nosqlbench/engine/api/util/Colors.java
+++ engine-api/src/main/java/io/nosqlbench/engine/api/util/Colors.java
@@ -21,36 +21,40 @@ package io.nosqlbench.engine.api.util;
  */
 public class Colors {
     public static String ANSI_Black = (char) 27 + "[30m";
-    public static String ANSI_BlackBG = (char) 27 + "[40m";
     public static String ANSI_Red = (char) 27 + "[31m";
-    public static String ANSI_RedBG = (char) 27 + "[41m";
     public static String ANSI_Green = (char) 27 + "[32m";
-    public static String ANSI_GreenBG = (char) 27 + "[42m";
     public static String ANSI_Yellow = (char) 27 + "[33m";
-    public static String ANSI_YellowBG = (char) 27 + "[43m";
     public static String ANSI_Blue = (char) 27 + "[34m";
-    public static String ANSI_BlueBG = (char) 27 + "[44m";
     public static String ANSI_Magenta = (char) 27 + "[35m";
-    public static String ANSI_MagentaBG = (char) 27 + "[45m";
     public static String ANSI_Cyan = (char) 27 + "[36m";
-    public static String ANSI_CyanBG = (char) 27 + "[46m";
     public static String ANSI_White = (char) 27 + "[37m";
+
+    public static String ANSI_BlackBG = (char) 27 + "[40m";
+    public static String ANSI_RedBG = (char) 27 + "[41m";
+    public static String ANSI_GreenBG = (char) 27 + "[42m";
+    public static String ANSI_YellowBG = (char) 27 + "[43m";
+    public static String ANSI_BlueBG = (char) 27 + "[44m";
+    public static String ANSI_MagentaBG = (char) 27 + "[45m";
+    public static String ANSI_CyanBG = (char) 27 + "[46m";
     public static String ANSI_WhiteBG = (char) 27 + "[47m";
+
     public static String ANSI_BrightBlack = (char) 27 + "[90m";
-    public static String ANSI_BrightBlackBG = (char) 27 + "[100m";
     public static String ANSI_BrightRed = (char) 27 + "[91m";
-    public static String ANSI_BrightRedBG = (char) 27 + "[101m";
     public static String ANSI_BrightGreen = (char) 27 + "[92m";
-    public static String ANSI_BrightGreenBG = (char) 27 + "[102m";
     public static String ANSI_BrightYellow = (char) 27 + "[93m";
-    public static String ANSI_BrightYellowBG = (char) 27 + "[103m";
     public static String ANSI_BrightBlue = (char) 27 + "[94m";
-    public static String ANSI_BrightBlueBG = (char) 27 + "[104m";
     public static String ANSI_BrightMagenta = (char) 27 + "[95m";
-    public static String ANSI_BrightMagentaBG = (char) 27 + "[105m";
     public static String ANSI_BrightCyan = (char) 27 + "[96m";
-    public static String ANSI_BrightCyanBG = (char) 27 + "[106m";
     public static String ANSI_BrightWhite = (char) 27 + "[97m";
+
+    public static String ANSI_BrightBlackBG = (char) 27 + "[100m";
+    public static String ANSI_BrightRedBG = (char) 27 + "[101m";
+    public static String ANSI_BrightGreenBG = (char) 27 + "[102m";
+    public static String ANSI_BrightYellowBG = (char) 27 + "[103m";
+    public static String ANSI_BrightBlueBG = (char) 27 + "[104m";
+    public static String ANSI_BrightMagentaBG = (char) 27 + "[105m";
+    public static String ANSI_BrightCyanBG = (char) 27 + "[106m";
     public static String ANSI_BrightWhiteBG = (char) 27 + "[107m";
-    public static String ANSI_Reset = (char) 27 + "[49m";
+
+    public static String ANSI_Reset = (char) 27 + "[39;49m";
 }
