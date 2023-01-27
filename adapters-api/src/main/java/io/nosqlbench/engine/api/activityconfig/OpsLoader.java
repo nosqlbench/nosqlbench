b9005a3a68e026af4678365f8655813d8e2c8100
Jonathan Shook <jshook@gmail.com>
2023-01-26 22:13:41 -0600

    add missing mutability+diff check. IntelliJ does not understand this one.

--- adapters-api/src/main/java/io/nosqlbench/engine/api/activityconfig/OpsLoader.java
+++ adapters-api/src/main/java/io/nosqlbench/engine/api/activityconfig/OpsLoader.java
@@ -80,7 +80,9 @@ public class OpsLoader {
 
         transformer.checkpointAccesses().forEach((k, v) -> {
             layered.addTemplateVariable(k, v);
-            params.remove(k);
+            if (params.containsKey(k)) {
+                params.remove(k);
+            }
         });
 
         return layered;
