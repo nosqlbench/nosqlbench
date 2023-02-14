973bdc57f54cd9156d710280595e77ea08648b30
Jonathan Shook <jshook@gmail.com>
2023-02-14 17:49:10 -0600

    clarify naming of state cache

--- virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/core/threadstate/SharedState.java
+++ virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/core/threadstate/SharedState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 nosqlbench
+ * Copyright (c) 2022-2023 nosqlbench
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * This provides common thread local instancing for sharing a thread local map across classes.
+ * This is being described as a <em>Thread Local State Cache</em>.
  */
 public class SharedState {
 
