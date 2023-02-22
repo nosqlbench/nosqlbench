83bc1843c86dec97ff0bdf6a31b42d6ff8fd0687
Jonathan Shook <jshook@gmail.com>
2023-02-22 15:59:18 -0600

    add tests

---
+++ virtdata-api/src/test/java/io/nosqlbench/virtdata/core/templates/CapturePointParserTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.nosqlbench.virtdata.core.templates;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CapturePointParserTest {
+
+    @Test
+    public void testCapturePoint1() {
+        CapturePointParser cpp = new CapturePointParser();
+        CapturePointParser.Result result = cpp.apply("string with [capture1]");
+        assertThat(result).isEqualTo(
+            new CapturePointParser.Result(
+                "string with capture1",
+                List.of(CapturePoint.of("capture1"))
+            )
+        );
+    }
+
+}
