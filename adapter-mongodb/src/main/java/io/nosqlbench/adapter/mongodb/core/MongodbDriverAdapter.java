6dafc1103e94f0bc041cc2a5f48c9a4ddd79dd41
jeffbanks <jeff.banks@datastax.com>
2023-04-03 16:58:59 -0500

    Removed state tracking and cleanup

--- adapter-mongodb/src/main/java/io/nosqlbench/adapter/mongodb/core/MongodbDriverAdapter.java
+++ adapter-mongodb/src/main/java/io/nosqlbench/adapter/mongodb/core/MongodbDriverAdapter.java
@@ -18,14 +18,11 @@ package io.nosqlbench.adapter.mongodb.core;
 
 import io.nosqlbench.api.config.standard.NBConfigModel;
 import io.nosqlbench.api.config.standard.NBConfiguration;
-import io.nosqlbench.api.config.standard.Param;
 import io.nosqlbench.engine.api.activityimpl.OpMapper;
 import io.nosqlbench.engine.api.activityimpl.uniform.BaseDriverAdapter;
 import io.nosqlbench.engine.api.activityimpl.uniform.DriverAdapter;
 import io.nosqlbench.engine.api.activityimpl.uniform.flowtypes.Op;
 import io.nosqlbench.nb.annotations.Service;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.function.Function;
 
@@ -35,47 +32,18 @@ import java.util.function.Function;
 @Service(value = DriverAdapter.class, selector = "mongodb")
 public class MongodbDriverAdapter extends BaseDriverAdapter<Op, MongoSpace> {
 
-    private static final Logger logger = LogManager.getLogger(MongodbDriverAdapter.class);
-    private String connection;
-
     @Override
     public OpMapper<Op> getOpMapper() {
-        return new MongoOpMapper(this);
+        return new MongoOpMapper(this, getConfiguration(), getSpaceCache());
     }
 
     @Override
     public Function<String, ? extends MongoSpace> getSpaceInitializer(NBConfiguration cfg) {
-        return s -> new MongoSpace(s, this.connection, cfg);
+        return spaceName -> new MongoSpace(spaceName, cfg);
     }
 
     @Override
     public NBConfigModel getConfigModel() {
-
-        NBConfigModel mongoConfigModel = MongoSpace.getConfigModel();
-        NBConfigModel parentModel = super.getConfigModel();
-        final NBConfigModel merged = mongoConfigModel.add(parentModel);
-
-        if (logger.isDebugEnabled()) {
-            logger.debug("mongo params:");
-            for (Param<?> param : mongoConfigModel.getParams()) {
-                logger.debug("param: {}", param);
-            }
-
-            logger.debug("parent params:");
-            for (Param<?> param : parentModel.getParams()) {
-                logger.debug("param: {}", param);
-            }
-
-            logger.debug("merged params:");
-            for (Param<?> param : merged.getParams()) {
-                logger.debug("param: {}", param);
-            }
-        }
-        return merged;
-    }
-
-    public void setConnection(String connection) {
-        logger.debug("\n\n\n\nSetting connection: {}", connection);
-        this.connection = connection;
+        return super.getConfigModel().add(MongoSpace.getConfigModel());
     }
 }
