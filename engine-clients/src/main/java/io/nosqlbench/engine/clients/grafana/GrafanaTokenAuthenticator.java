48d379b05dfe426f3a200f95f86825d80911a0a2
Jonathan Shook <jshook@gmail.com>
2023-08-28 20:42:33 -0500

    add missing no-args ctor

--- engine-clients/src/main/java/io/nosqlbench/engine/clients/grafana/GrafanaTokenAuthenticator.java
+++ engine-clients/src/main/java/io/nosqlbench/engine/clients/grafana/GrafanaTokenAuthenticator.java
@@ -34,4 +34,8 @@ public class GrafanaTokenAuthenticator implements BundledApp {
     public int applyAsInt(String[] value) {
         return 0;
     }
+
+    public GrafanaTokenAuthenticator() {
+
+    }
 }
