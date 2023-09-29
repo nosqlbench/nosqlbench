e79db4ea0572ca5373450ef5d72b7ffd19785d9d
Jonathan Shook <jshook@gmail.com>
2023-09-29 02:01:28 -0500

    component scaffolding

---
+++ nb-api/src/test/java/io/nosqlbench/components/NBComponentTraversalTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2023 nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.nosqlbench.components;
+
+import io.nosqlbench.api.config.standard.TestComponent;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NBComponentTraversalTest {
+
+    private final static TestComponent a = new TestComponent("name_a","a");
+    private final static TestComponent sub1 = new TestComponent(a, "name_1", "1");
+    private final static TestComponent sub2 = new TestComponent(a, "name_2", "2");
+    private final static TestComponent dotX = new TestComponent(sub1, "name_X", "X");
+    private final static TestComponent dotY = new TestComponent(sub1, "name_Y", "Y");
+
+
+    @Test
+    public void testDepthFirstTraversal() {
+        Iterator<NBComponent> byDepth = NBComponentTraversal.traverseDepth(a);
+        ArrayList<NBComponent> taller = new ArrayList<>();
+        byDepth.forEachRemaining(taller::add);
+        assertThat(taller).containsExactly(a,sub1,dotX,dotY,sub2);
+    }
+    @Test
+    public void testBreadthFirstTraversal() {
+        Iterator<NBComponent> byBreadth = NBComponentTraversal.traverseBreadth(a);
+        ArrayList<NBComponent> wider = new ArrayList<>();
+        byBreadth.forEachRemaining(wider::add);
+        assertThat(wider).containsExactly(a,sub1,sub2,dotX,dotY);
+    }
+
+}
