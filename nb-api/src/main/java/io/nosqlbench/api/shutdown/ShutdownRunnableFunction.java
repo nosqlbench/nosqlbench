235c3d8296be00457e55de52ceeee908a25b8cd8
Mark Wolters <mark.wolters@datastax.com>
2023-10-05 14:47:57 -0400

    shutdown hook plugin

--- nb-api/src/main/java/io/nosqlbench/api/shutdown/ShutdownRunnableFunction.java
+++ nb-api/src/main/java/io/nosqlbench/api/shutdown/ShutdownRunnableFunction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 nosqlbench
+ * Copyright (c) 2022-2023 nosqlbench
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package io.nosqlbench.engine.shutdown;
+package io.nosqlbench.api.shutdown;
 
 import org.apache.logging.log4j.Logger;
 
