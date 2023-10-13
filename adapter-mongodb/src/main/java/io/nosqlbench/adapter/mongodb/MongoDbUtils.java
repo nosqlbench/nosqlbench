77ae11c925b8063b01acf29f2dd9327b5f77e7af
Jonathan Shook <jshook@gmail.com>
2023-10-13 17:51:03 -0500

    mongodb utils to convert document fields to vector indices

---
+++ adapter-mongodb/src/main/java/io/nosqlbench/adapter/mongodb/MongoDbUtils.java
@@ -0,0 +1,18 @@
+package io.nosqlbench.adapter.mongodb;
+
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDbUtils {
+    public static int[] getFieldFromResults(String field, Document document) {
+        List<Document> value = document.getList(field, Document.class);
+        List<String> keyStrings = new ArrayList<>();
+        for (Document matchingVector : value) {
+            keyStrings.add(matchingVector.get("key",String.class));
+        }
+        return keyStrings.stream().mapToInt(Integer::parseInt).toArray();
+    }
+}
