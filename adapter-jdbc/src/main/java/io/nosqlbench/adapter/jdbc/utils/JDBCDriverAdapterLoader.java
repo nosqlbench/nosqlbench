35fa137633c8e00541e955668e119b9ea7e63c37
Jonathan Shook <jshook@gmail.com>
2023-10-18 17:12:29 -0500

    fixup jdbc after merging into 5.21

--- adapter-jdbc/src/main/java/io/nosqlbench/adapter/jdbc/utils/JDBCDriverAdapterLoader.java
+++ adapter-jdbc/src/main/java/io/nosqlbench/adapter/jdbc/utils/JDBCDriverAdapterLoader.java
@@ -1,12 +1,14 @@
+package io.nosqlbench.adapter.jdbc.utils;
+
 /*
  * Copyright (c) 2022 nosqlbench
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,3 +17,17 @@
  * under the License.
  */
 
+
+import io.nosqlbench.adapter.diag.DriverAdapterLoader;
+import io.nosqlbench.adapter.jdbc.JDBCDriverAdapter;
+import io.nosqlbench.api.labels.NBLabels;
+import io.nosqlbench.components.NBComponent;
+import io.nosqlbench.nb.annotations.Service;
+
+@Service(value = DriverAdapterLoader.class, selector = "jdbc")
+public class JDBCDriverAdapterLoader implements DriverAdapterLoader {
+    @Override
+    public JDBCDriverAdapter load(NBComponent parent, NBLabels childLabels) {
+        return new JDBCDriverAdapter(parent, childLabels);
+    }
+}
