77e599d5747e2e3875c8206c8007fe68dcbe676f
Jonathan Shook <jshook@gmail.com>
2024-02-24 12:19:48 -0600

    customizations which need to be generalized before merge

--- adapter-http/src/main/java/io/nosqlbench/adapter/http/JsonElementUtils.java
+++ adapter-http/src/main/java/io/nosqlbench/adapter/http/JsonElementUtils.java
@@ -2,13 +2,13 @@ package io.nosqlbench.adapter.http;
 
 /*
  * Copyright (c) 2022 nosqlbench
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -22,6 +22,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,6 +62,36 @@ public class JsonElementUtils {
             i++;
         }
         return keys;
-
     }
+
+    public static List<Float> customNumberArrayToFloatList(JsonElement element) {
+        JsonObject o1 = element.getAsJsonObject();
+        JsonElement data = o1.get("data");
+        JsonArray dary = data.getAsJsonArray();
+        JsonElement element0 = dary.get(0);
+        JsonObject eobj1 = element0.getAsJsonObject();
+        JsonElement embedding = eobj1.get("embedding");
+        JsonArray ary = embedding.getAsJsonArray();
+        ArrayList<Float> list = new ArrayList<>(ary.size());
+        for (JsonElement jsonElement : ary) {
+            list.add(jsonElement.getAsFloat());
+        }
+        return list;
+    }
+
+    public static float[] customNumberArrayToFloatArray(JsonElement element) {
+        JsonObject o1 = element.getAsJsonObject();
+        JsonElement data = o1.get("data");
+        JsonArray dary = data.getAsJsonArray();
+        JsonElement element0 = dary.get(0);
+        JsonObject eobj1 = element0.getAsJsonObject();
+        JsonElement embedding = eobj1.get("embedding");
+        JsonArray ary = embedding.getAsJsonArray();
+        float[] floats = new float[ary.size()];
+        for (int i = 0; i < floats.length; i++) {
+            floats[i]=ary.get(i).getAsFloat();
+        }
+        return floats;
+    }
+
 }
