b128fbc225ab539e90e8b05c4afe31858a460bfe
Mark Wolters <mark.wolters@datastax.com>
2024-03-06 12:59:57 -0400

    updating tests

--- virtdata-lib-hdf5/src/test/java/io/nosqlbench/virtdata/predicates/adapter/JsonPredicateSerDesTest.java
+++ virtdata-lib-hdf5/src/test/java/io/nosqlbench/virtdata/predicates/adapter/JsonPredicateSerDesTest.java
@@ -18,11 +18,16 @@
 package io.nosqlbench.virtdata.predicates.adapter;
 
 import io.nosqlbench.virtdata.predicates.JsonPredicateSerDes;
+import io.nosqlbench.virtdata.predicates.ast.PConjunction;
+import io.nosqlbench.virtdata.predicates.ast.POperator;
 import io.nosqlbench.virtdata.predicates.ast.PredicateExpr;
+import io.nosqlbench.virtdata.predicates.ast.PredicateTerm;
 import io.nosqlbench.virtdata.predicates.types.PredicateSerDes;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonPredicateSerDesTest {
 
@@ -56,15 +61,58 @@ public class JsonPredicateSerDesTest {
     }
     """;
 
+    public static final String test3 = """
+    {
+        "conjunction": "or",
+        "terms": [
+            {
+                "field": {"name": "highprice"},
+                "operator": "gt",
+                "comparator": {"value": 1000}
+            },
+            {
+                "field": {"name": "lowprice"},
+                "operator": "lt",
+                "comparator": {"value": 1}
+            }
+        ]
+    }
+    """;
+
     private static final PredicateSerDes instance = new JsonPredicateSerDes();
 
     @Test
     public void testUnserialize() {
         PredicateExpr result1 = instance.unserialize(test1);
-        assertNotNull(result1);
+        assertEquals(PConjunction.none, result1.getConjunction());
+        List<PredicateTerm> terms = result1.getTerms();
+        assertEquals(1, terms.size());
+        assertEquals("firstname", terms.getFirst().field.name);
+        assertEquals("Mark", terms.getFirst().comparator.value);
+        assertEquals(POperator.eq, terms.getFirst().operator);
 
         PredicateExpr result2 = instance.unserialize(test2);
-        assertNotNull(result2);
+        assertEquals(PConjunction.and, result2.getConjunction());
+        List<PredicateTerm> terms2 = result2.getTerms();
+        assertEquals(2, terms2.size());
+        assertEquals("firstname", terms2.getFirst().field.name);
+        assertEquals("Mark", terms2.getFirst().comparator.value);
+        assertEquals(POperator.eq, terms2.getFirst().operator);
+        assertEquals("lastname", terms2.get(1).field.name);
+        assertEquals("Wolters", terms2.get(1).comparator.value);
+        assertEquals(POperator.eq, terms2.get(1).operator);
+
+
+        PredicateExpr result3 = instance.unserialize(test3);
+        assertEquals(PConjunction.or, result3.getConjunction());
+        List<PredicateTerm> terms3 = result3.getTerms();
+        assertEquals(2, terms3.size());
+        assertEquals("highprice", terms3.getFirst().field.name);
+        assertEquals(1000.0, terms3.getFirst().comparator.value);
+        assertEquals(POperator.gt, terms3.getFirst().operator);
+        assertEquals("lowprice", terms3.get(1).field.name);
+        assertEquals(1.0, terms3.get(1).comparator.value);
+        assertEquals(POperator.lt, terms3.get(1).operator);
 
     }
 
