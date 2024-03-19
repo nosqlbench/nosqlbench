fc574eceb27ea950e7ddab053d48afc4c4d3fe18
Mark Wolters <mark.wolters@datastax.com>
2024-03-19 14:06:30 -0400

    cleanup

--- nbr/src/main/java/io/nosqlbench/scenarios/simframe/optimizers/findmax/FindmaxConfig.java
+++ nbr/src/main/java/io/nosqlbench/scenarios/simframe/optimizers/findmax/FindmaxConfig.java
@@ -21,9 +21,9 @@ import io.nosqlbench.engine.core.lifecycle.scenario.container.NBCommandParams;
 public class FindmaxConfig {
     private double sample_time_ms;
     private double sample_max;
-    private double rate_base;
-    private double rate_step;
-    private double rate_incr;
+    private double base_value;
+    private double step_value;
+    private double value_incr;
     private double sample_incr;
     private long min_settling_ms;
 
@@ -35,16 +35,16 @@ public class FindmaxConfig {
         return sample_max;
     }
 
-    public double rate_base() {
-        return rate_base;
+    public double base_value() {
+        return base_value;
     }
 
-    public double rate_step() {
-        return rate_step;
+    public double step_value() {
+        return step_value;
     }
 
-    public double rate_incr() {
-        return rate_incr;
+    public double value_incr() {
+        return value_incr;
     }
 
     public double sample_incr() {
@@ -63,20 +63,20 @@ public class FindmaxConfig {
         this.sample_max = sample_max;
     }
 
-    public void setRate_base(double rate_base) {
-        this.rate_base = rate_base;
+    public void setBase_value(double base_value) {
+        this.base_value = base_value;
     }
 
-    public void setRate_step(double rate_step) {
-        this.rate_step = rate_step;
+    public void setStep_value(double step_value) {
+        this.step_value = step_value;
     }
 
     public void setSample_incr(double sample_incr) {
         this.sample_incr = sample_incr;
     }
 
-    public void setRate_incr(double rate_incr) {
-        this.rate_incr = rate_incr;
+    public void setValue_incr(double value_incr) {
+        this.value_incr = value_incr;
     }
 
     public void setMin_settling_ms(long min_settling_ms) {
@@ -84,12 +84,12 @@ public class FindmaxConfig {
     }
 
     public FindmaxConfig(NBCommandParams params) {
-        params.maybeGet("sample_time_ms").map(Double::parseDouble).orElse(4000d);
-        params.maybeGet("sample_max").map(Double::parseDouble).orElse(10000d);
-        params.maybeGet("rate_base").map(Double::parseDouble).orElse(10d);
-        params.maybeGet("rate_step").map(Double::parseDouble).orElse(100d);
-        params.maybeGet("rate_incr").map(Double::parseDouble).orElse(2d);
-        params.maybeGet("sample_incr").map(Double::parseDouble).orElse(1.2d);
-        params.maybeGet("min_settling_ms").map(Long::parseLong).orElse(4000L);
+        setSample_time_ms(params.maybeGet("sample_time_ms").map(Double::parseDouble).orElse(4000d));
+        setSample_max(params.maybeGet("sample_max").map(Double::parseDouble).orElse(10000d));
+        setBase_value(params.maybeGet("base_value").map(Double::parseDouble).orElse(10d));
+        setStep_value(params.maybeGet("step_value").map(Double::parseDouble).orElse(100d));
+        setValue_incr(params.maybeGet("value_incr").map(Double::parseDouble).orElse(2d));
+        setSample_incr(params.maybeGet("sample_incr").map(Double::parseDouble).orElse(1.2d));
+        setMin_settling_ms(params.maybeGet("min_settling_ms").map(Long::parseLong).orElse(4000L));
     }
 }
