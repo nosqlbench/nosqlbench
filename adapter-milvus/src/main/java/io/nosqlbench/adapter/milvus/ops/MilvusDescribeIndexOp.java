1319f9842de9c3fe6e2853471ba04316b6e2524c
Jonathan Shook <jshook@gmail.com>
2024-03-27 10:44:52 -0500

    stability improvements for milvus describe index poller

--- adapter-milvus/src/main/java/io/nosqlbench/adapter/milvus/ops/MilvusDescribeIndexOp.java
+++ adapter-milvus/src/main/java/io/nosqlbench/adapter/milvus/ops/MilvusDescribeIndexOp.java
@@ -27,13 +27,14 @@ import io.nosqlbench.adapters.api.activityimpl.uniform.flowtypes.OpGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.LockSupport;
 
 public class MilvusDescribeIndexOp extends MilvusBaseOp<DescribeIndexParam> implements OpGenerator {
     private final boolean doPollTillIndexed;
     private final int awaitIndexTries;
     private int tried = 0;
     private MilvusDescribeIndexOp nextOp;
-    private long lastAttemptAt=0L;
+    private long lastAttemptAt = 0L;
 
     public MilvusDescribeIndexOp(
         MilvusServiceClient client,
@@ -50,26 +51,27 @@ public class MilvusDescribeIndexOp extends MilvusBaseOp<DescribeIndexParam> impl
     public Object applyOp(long value) {
         long attemptAt = System.currentTimeMillis();
         long gap = attemptAt - lastAttemptAt;
-        if (gap<500) {
-            logger.warn("You are polling the state of indexes with an interval of only " + gap + "ms.");
+        if (gap < 500) {
+            logger.warn("You are polling index state at " + gap + "ms interval. Forcing 1S delay.");
+            LockSupport.parkNanos(1_000_000_000L);
         }
-        lastAttemptAt=attemptAt;
+        lastAttemptAt = attemptAt;
 
         R<DescribeIndexResponse> describeIndexResponseR = client.describeIndex(request);
         tried++;
         DescribeIndexResponse data = describeIndexResponseR.getData();
 
         if (doPollTillIndexed) {
+            this.nextOp = null;
             List<IndexStats> stats = getIndexStats(data);
             int maxpct = stats.stream().mapToInt(IndexStats::percent).max().orElse(100);
-            if (maxpct == 100) {
-                logger.info("indexing percent at " + maxpct + " on try " + tried);
-                this.nextOp=null;
-            } else if (awaitIndexTries<tried) {
-                logger.info("indexing percent at " + maxpct + " on try " + tried + ", retrying");
+            if (maxpct < 100 && tried < awaitIndexTries) {
+                logger.info("indexing at " + maxpct + "% on try " + tried + "/" + awaitIndexTries + ", retrying");
                 this.nextOp = this;
-            } else {
-                logger.info("indexing percent at " + maxpct + " on try " + tried + ", throwing error");
+            } else if (maxpct >= 100) {
+                logger.info("indexing at " + maxpct + "% on try " + tried + "/" + awaitIndexTries + ", complete");
+            } else { // tried >= awaitIndexTries
+                logger.info("indexing  at " + maxpct + " on try " + tried + "/" + awaitIndexTries + ", throwing error");
                 throw new MilvusIndexingIncompleteError(request, tried, stats);
             }
         }
@@ -79,7 +81,7 @@ public class MilvusDescribeIndexOp extends MilvusBaseOp<DescribeIndexParam> impl
     private List<IndexStats> getIndexStats(DescribeIndexResponse data) {
         var stats = new ArrayList<IndexStats>();
         for (IndexDescription desc : data.getIndexDescriptionsList()) {
-            stats.add(new IndexStats(desc.getIndexName(),desc.getIndexedRows(),desc.getPendingIndexRows()));
+            stats.add(new IndexStats(desc.getIndexName(), desc.getIndexedRows(), desc.getPendingIndexRows()));
         }
         return stats;
     }
@@ -90,10 +92,10 @@ public class MilvusDescribeIndexOp extends MilvusBaseOp<DescribeIndexParam> impl
         long pending_rows
     ) {
         public int percent() {
-            if (pending_rows==0) {
+            if (pending_rows == 0) {
                 return 100;
             }
-            return (int) (100.0d*((double)indexed_rows/(double)(indexed_rows+pending_rows)));
+            return (int) (100.0d * ((double) indexed_rows / (double) (indexed_rows + pending_rows)));
         }
     }
 
