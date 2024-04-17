3fab415c6da5e1070444c98941c0e46b14ec8afd
Mark Wolters <mark.wolters@datastax.com>
2024-04-17 09:56:55 -0400

    added min_frames and contra_base_value params

--- nbr/src/main/java/io/nosqlbench/scenarios/simframe/optimizers/findmax/CMD_findmax.java
+++ nbr/src/main/java/io/nosqlbench/scenarios/simframe/optimizers/findmax/CMD_findmax.java
@@ -57,7 +57,7 @@ public class CMD_findmax extends NBBaseCommand {
 
         FindmaxConfig findmaxConfig = new FindmaxConfig(params);
         switch(findmaxConfig.optimization_type()) {
-            case "rate" ->
+            case "rate" -> {
                 model.add("rate",
                     findmaxConfig.min_value(),      // min
                     findmaxConfig.base_value(),     // initial
@@ -67,13 +67,24 @@ public class CMD_findmax extends NBBaseCommand {
                         1.1d,
                         SimRateSpec.Verb.restart)))
                 );
-            case "threads" ->
+                if (findmaxConfig.contra_base_value() != 0) {
+                    flywheel.onEvent(ParamChange.of(new SetThreads((int) (findmaxConfig.contra_base_value()))));
+                }
+            }
+            case "threads" -> {
                 model.add("threads",
                     findmaxConfig.min_value(),      // min
                     findmaxConfig.base_value(),     // initial
                     findmaxConfig.max_value(),      // max
                     threads -> flywheel.onEvent(ParamChange.of(new SetThreads((int) (threads))))
                 );
+                if (findmaxConfig.contra_base_value() != 0) {
+                    flywheel.onEvent(ParamChange.of(new CycleRateSpec(
+                        findmaxConfig.contra_base_value(),
+                        1.1d,
+                        SimRateSpec.Verb.restart)));
+                }
+            }
             default ->
                 throw new RuntimeException("Unsupported optimization type: " + findmaxConfig.optimization_type());
         }
