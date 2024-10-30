f6cbd5e42b3158ee06bee18ea8a049c47dddeba7
Jonathan Shook <jshook@gmail.com>
2024-10-30 16:12:21 -0500

    update copyright with no date

--- nb-adapters/adapter-milvus/src/main/java/io/nosqlbench/adapter/milvus/opdispensers/MilvusDeleteOpDispenser.java
+++ nb-adapters/adapter-milvus/src/main/java/io/nosqlbench/adapter/milvus/opdispensers/MilvusDeleteOpDispenser.java
@@ -1,7 +1,7 @@
 package io.nosqlbench.adapter.milvus.opdispensers;
 
 /*
- * Copyright (c) 2024 nosqlbench
+ * Copyright (c) nosqlbench
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- nb-virtdata/virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/conversions/from_string/FromCSVToBoxedFloatArray.java
+++ nb-virtdata/virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/conversions/from_string/FromCSVToBoxedFloatArray.java
@@ -1,14 +1,14 @@
 package io.nosqlbench.virtdata.library.basics.shared.conversions.from_string;
 
 /*
- * Copyright (c) 2022 nosqlbench
- * 
+ * Copyright (c) nosqlbench
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
