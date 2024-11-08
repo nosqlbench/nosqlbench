2bad7dc63c095a68c7fc250f5e57fe53af736aa3
Jonathan Shook <jshook@gmail.com>
2024-11-08 12:35:18 -0600

    document alignment of log levels and advisor levels

--- nb-apis/nb-api/src/main/java/io/nosqlbench/nb/api/advisor/conditions/Conditions.java
+++ nb-apis/nb-api/src/main/java/io/nosqlbench/nb/api/advisor/conditions/Conditions.java
@@ -2,13 +2,13 @@ package io.nosqlbench.nb.api.advisor.conditions;
 
 /*
  * Copyright (c) nosqlbench
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,6 +21,27 @@ package io.nosqlbench.nb.api.advisor.conditions;
 import io.nosqlbench.nb.api.advisor.NBAdvisorCondition;
 import org.apache.logging.log4j.Level;
 
+/**
+ * <P>The mappings between logging levels for conditions and the advisory
+ * levels should be consistent, so they will be described here to start.</P>
+ *
+ * <P>These are the levels which conditions should use:</P>
+ * <OL>
+ *     <LI>Level.ERROR (internal to conditions, cause immediate exceptions)</LI>
+ *     <LI>Level.WARN (conditions which should inform the user about a likely issue)</LI>
+ *     <LI>Level.INFO (conditions which may inform the user about a possible issue)</LI>
+ * </OL>
+ *
+ * <P>This means that the following three behaviors are possible:
+ * <OL>
+ *     <LI>{@link io.nosqlbench.nb.api.advisor.NBAdvisorLevel#none} - Only ERROR level results throw exceptions, no
+ *     results are presented.</LI>
+ *     <LI>{@link io.nosqlbench.nb.api.advisor.NBAdvisorLevel#validate} - Only ERROR level results throw exceptions,
+ *     all results are presented. </LI>
+ *     <LI>{@link io.nosqlbench.nb.api.advisor.NBAdvisorLevel#enforce} - ERROR and WARN levels throw exceptions.</LI>
+ * </P>
+ *
+ */
 public class Conditions {
 
     public static NoHyphens NoHyphensError = new NoHyphens(Level.ERROR);
