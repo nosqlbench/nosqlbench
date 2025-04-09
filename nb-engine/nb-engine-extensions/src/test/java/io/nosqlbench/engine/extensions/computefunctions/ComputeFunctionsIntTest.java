3b606f6279a883ae3805597a6845da4c74430ecd
Jonathan Shook <jshook@gmail.com>
2025-04-09 13:07:11 -0500

    make test variables more obvious

--- nb-engine/nb-engine-extensions/src/test/java/io/nosqlbench/engine/extensions/computefunctions/ComputeFunctionsIntTest.java
+++ nb-engine/nb-engine-extensions/src/test/java/io/nosqlbench/engine/extensions/computefunctions/ComputeFunctionsIntTest.java
@@ -27,40 +27,40 @@ class ComputeFunctionsIntTest {
 
     private final static Offset<Double> offset=Offset.offset(0.001d);
     private final static int[] allInts =new int[]{0,1,2,3,4,5,6,7,8,9};
-    private final static int[] oddInts37195 = new int[]{3,7,1,9,5};
-    private final static int[] evenInts86204 = new int[]{8,6,2,0,4};
-    private final static int[] lowInts01234 = new int[]{0,1,2,3,4};
-    private final static int[] highInts56789 = new int[]{5,6,7,8,9};
+    private final static int[] int_3_7_1_9_5 = new int[]{3, 7, 1, 9, 5};
+    private final static int[] int_8_6_2_0_4 = new int[]{8, 6, 2, 0, 4};
+    private final static int[] int_0_1_2_3_4 = new int[]{0, 1, 2, 3, 4};
+    private final static int[] int_5_6_7_8_9 = new int[]{5, 6, 7, 8, 9};
 
-    private final static int[] intsBy3_369 = new int[]{3,6,9};
-    private final static int[] intsBy3_369_12_15 = new int[]{3,6,9,12,15};
-    private final static int[] intsBy3_693 = new int[]{6,9,3};
-    private final static int[] midInts45678 = new int[]{4,5,6,7,8};
-    private final static int[] ints12390 = new int[]{1,2,3,9,0};
+    private final static int[] int_3_6_9 = new int[]{3, 6, 9};
+    private final static int[] int_3_6_9_12_15 = new int[]{3, 6, 9, 12, 15};
+    private final static int[] int_6_9_3 = new int[]{6, 9, 3};
+    private final static int[] int_4_5_6_7_8 = new int[]{4, 5, 6, 7, 8};
+    private final static int[] int_1_2_3_9_0 = new int[]{1, 2, 3, 9, 0};
 
     @Test
     void testRecallIntArrays() {
-        assertThat(ComputeFunctions.recall(evenInts86204,oddInts37195, 5))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_7_1_9_5, 5))
             .as("finding 0 actual of any should yield recall=0.0")
             .isCloseTo(0.0d, offset);
 
-        assertThat(ComputeFunctions.recall(evenInts86204,intsBy3_369_12_15, 5))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_6_9_12_15, 5))
             .as("finding 1 actual of 5 relevant should yield recall=0.2")
             .isCloseTo(0.2d, offset);
 
-        assertThat(ComputeFunctions.recall(evenInts86204,intsBy3_369_12_15,5))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_6_9_12_15,5))
             .as("finding 0 (limited) actual of 5 relevant should yield recall=0.0")
             .isCloseTo(0.2d, offset);
 
-        assertThat(ComputeFunctions.recall(evenInts86204,intsBy3_369_12_15,1))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_6_9_12_15,1))
             .as("finding 1 (limited) actual of 1 (limited) relevant should yield recall=1.0")
             .isCloseTo(1.0d, offset);
 
-        assertThat(ComputeFunctions.recall(evenInts86204,intsBy3_369_12_15,2))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_6_9_12_15,2))
             .as("finding 1 (limited) actual of 2 (limited) relevant should yield recall=0.5")
             .isCloseTo(0.5d, offset);
 
-        assertThat(ComputeFunctions.recall(evenInts86204,intsBy3_369_12_15,3))
+        assertThat(ComputeFunctions.recall(int_8_6_2_0_4, int_3_6_9_12_15,3))
             .as("finding 1 (limited) actual of 3 (limited) relevant should yield recall=0.3...")
             .isCloseTo(0.3333d, offset);
 
@@ -68,13 +68,13 @@ class ComputeFunctionsIntTest {
 
     @Test
     void testPrecisionIntArrays() {
-        assertThat(ComputeFunctions.precision(evenInts86204,intsBy3_693))
+        assertThat(ComputeFunctions.precision(int_8_6_2_0_4, int_6_9_3))
             .as("one of three results being relevant should yield precision=0.333")
             .isCloseTo(0.333,offset);
-        assertThat(ComputeFunctions.precision(evenInts86204,lowInts01234))
+        assertThat(ComputeFunctions.precision(int_8_6_2_0_4, int_0_1_2_3_4))
             .as("three of five results being relevant should yield precision=0.6")
             .isCloseTo(0.6,offset);
-        assertThat(ComputeFunctions.precision(evenInts86204,oddInts37195))
+        assertThat(ComputeFunctions.precision(int_8_6_2_0_4, int_3_7_1_9_5))
             .as("none of the results being relevant should yield precision=0.0")
             .isCloseTo(0.0,offset);
     }
@@ -96,30 +96,30 @@ class ComputeFunctionsIntTest {
 
     @Test
     public void testReciprocalRank() {
-        assertThat(ComputeFunctions.reciprocal_rank(intsBy3_369,highInts56789))
+        assertThat(ComputeFunctions.reciprocal_rank(int_3_6_9, int_5_6_7_8_9))
             .as("relevant results in rank 2 should yield RR=0.5")
             .isCloseTo(0.5d,offset);
 
-        assertThat(ComputeFunctions.reciprocal_rank(highInts56789,lowInts01234))
+        assertThat(ComputeFunctions.reciprocal_rank(int_5_6_7_8_9, int_0_1_2_3_4))
             .as("no relevant results should yield RR=0.0")
             .isCloseTo(0.0d,offset);
     }
 
     @Test
     public void testIntegerIntersection() {
-        int[] result = Intersections.find(lowInts01234,midInts45678);
+        int[] result = Intersections.find(int_0_1_2_3_4, int_4_5_6_7_8);
         assertThat(result).isEqualTo(new int[]{4});
     }
 
     @Test
     public void testCountIntIntersection() {
-        int result = Intersections.count(oddInts37195, ints12390);
+        int result = Intersections.count(int_3_7_1_9_5, int_1_2_3_9_0);
         assertThat(result).isEqualTo(2L);
     }
 
     @Test
     public void tesTCountIntersectionDepth() {
-        assertThat(Intersections.count(oddInts37195, ints12390,0)).isEqualTo(0);;
+        assertThat(Intersections.count(int_3_7_1_9_5, int_1_2_3_9_0,0)).isEqualTo(0);;
     }
 
     @Test
@@ -129,7 +129,7 @@ class ComputeFunctionsIntTest {
             .as("")
             .isCloseTo(0.755d,offset);
 
-        double ap2 = ComputeFunctions.average_precision(ints12390, intsBy3_369);
+        double ap2 = ComputeFunctions.average_precision(int_1_2_3_9_0, int_3_6_9);
         assertThat(ap2)
             .as("")
             .isCloseTo(0.833,offset);
--- nb-engine/nb-engine-extensions/src/test/java/io/nosqlbench/engine/extensions/computefunctions/ComputeFunctionsLongTest.java
+++ nb-engine/nb-engine-extensions/src/test/java/io/nosqlbench/engine/extensions/computefunctions/ComputeFunctionsLongTest.java
@@ -27,41 +27,41 @@ class ComputeFunctionsLongTest {
 
     private final static Offset<Double> offset=Offset.offset(0.001d);
     private final static long[] longs_0to9 =new long[]{0,1,2,3,4,5,6,7,8,9};
-    private final static long[] longs_37195 = new long[]{3,7,1,9,5};
-    private final static long[] longs_86204 = new long[]{8,6,2,0,4};
-    private final static long[] longs_01234 = new long[]{0,1,2,3,4};
-    private final static long[] longs_56789 = new long[]{5,6,7,8,9};
+    private final static long[] longs_3_7_1_9_5 = new long[]{3, 7, 1, 9, 5};
+    private final static long[] longs_8_6_2_0_4 = new long[]{8, 6, 2, 0, 4};
+    private final static long[] longs_0_1_2_3_4 = new long[]{0, 1, 2, 3, 4};
+    private final static long[] longs_5_6_7_8_9 = new long[]{5, 6, 7, 8, 9};
 
-    private final static long[] longs_369 = new long[]{3,6,9};
-    private final static long[] longs_369_12_15 = new long[]{3,6,9,12,15};
-    private final static long[] longs_693 = new long[]{6,9,3};
-    private final static long[] longs_45678 = new long[]{4,5,6,7,8};
-    private final static long[] longs_12390 = new long[]{1,2,3,9,0};
+    private final static long[] longs_3_6_9 = new long[]{3, 6, 9};
+    private final static long[] longs_3_6_9_12_15 = new long[]{3, 6, 9, 12, 15};
+    private final static long[] longs_6_9_3 = new long[]{6, 9, 3};
+    private final static long[] longs_4_5_6_7_8 = new long[]{4, 5, 6, 7, 8};
+    private final static long[] longs_1_2_3_9_0 = new long[]{1, 2, 3, 9, 0};
 
     @Test
     void testRecallLongArrays() {
-        assertThat(ComputeFunctions.recall(longs_86204, longs_37195, 5))
+        assertThat(ComputeFunctions.recall(longs_8_6_2_0_4, longs_3_7_1_9_5, 5))
             .as("finding 0 actual of any should yield recall=0.0")
             .isCloseTo(0.0d, offset);
 
-        assertThat(ComputeFunctions.recall(longs_86204, longs_369_12_15, 5))
+        assertThat(ComputeFunctions.recall(longs_8_6_2_0_4, longs_3_6_9_12_15, 5))
             .as("finding 1 actual of 5 relevant should yield recall=0.2")
             .isCloseTo(0.2d, offset);
 
-        assertThat(ComputeFunctions.recall(longs_86204, longs_369_12_15,5))
+        assertThat(ComputeFunctions.recall(longs_8_6_2_0_4, longs_3_6_9_12_15,5))
             .as("finding 1 (limited) actual of 5 relevant should yield recall=0.0")
             .isCloseTo(0.2d, offset);
     }
 
     @Test
     void testPrecisionLongArrays() {
-        assertThat(ComputeFunctions.precision(longs_86204, longs_693))
+        assertThat(ComputeFunctions.precision(longs_8_6_2_0_4, longs_6_9_3))
             .as("one of three results being relevant should yield precision=0.333")
             .isCloseTo(0.333,offset);
-        assertThat(ComputeFunctions.precision(longs_86204, longs_01234))
+        assertThat(ComputeFunctions.precision(longs_8_6_2_0_4, longs_0_1_2_3_4))
             .as("three of five results being relevant should yield precision=0.6")
             .isCloseTo(0.6,offset);
-        assertThat(ComputeFunctions.precision(longs_86204, longs_37195))
+        assertThat(ComputeFunctions.precision(longs_8_6_2_0_4, longs_3_7_1_9_5))
             .as("none of the results being relevant should yield precision=0.0")
             .isCloseTo(0.0,offset);
     }
@@ -81,18 +81,18 @@ class ComputeFunctionsLongTest {
 
     @Test
     public void testReciprocalRankLongs() {
-        assertThat(ComputeFunctions.reciprocal_rank(longs_369, longs_56789))
+        assertThat(ComputeFunctions.reciprocal_rank(longs_3_6_9, longs_5_6_7_8_9))
             .as("relevant results in rank 2 should yield RR=0.5")
             .isCloseTo(0.5d,offset);
 
-        assertThat(ComputeFunctions.reciprocal_rank(longs_56789, longs_01234))
+        assertThat(ComputeFunctions.reciprocal_rank(longs_5_6_7_8_9, longs_0_1_2_3_4))
             .as("no relevant results should yield RR=0.0")
             .isCloseTo(0.0d,offset);
     }
 
     @Test
     public void testCountLongIntersection() {
-        long result = Intersections.count(longs_37195, longs_12390);
+        long result = Intersections.count(longs_3_7_1_9_5, longs_1_2_3_9_0);
         assertThat(result).isEqualTo(3L);
     }
 
