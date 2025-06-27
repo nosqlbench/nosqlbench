ea303291e1de706568acd5c449a6a68e817d3311
Jonathan Shook <jshook@gmail.com>
2025-06-26 20:13:24 -0500

    preserve non-numeric space names

--- nb-apis/adapters-api/src/main/java/io/nosqlbench/adapters/api/activityimpl/uniform/BaseSpace.java
+++ nb-apis/adapters-api/src/main/java/io/nosqlbench/adapters/api/activityimpl/uniform/BaseSpace.java
@@ -31,11 +31,37 @@ import io.nosqlbench.adapters.api.activityimpl.uniform.flowtypes.CycleOp;
 public class BaseSpace<SelfT extends BaseSpace<SelfT> > implements Space {
 
     private final String spaceName;
+    private String originalName;
     private final DriverAdapter<?, SelfT> adapter;
 
     public BaseSpace(DriverAdapter<?,SelfT> adapter, long idx) {
+        this(adapter, idx, String.valueOf(idx));
+    }
+
+    public BaseSpace(DriverAdapter<?,SelfT> adapter, long idx, String originalName) {
         this.spaceName = String.valueOf(idx);
         this.adapter = adapter;
+        this.originalName = originalName;
+    }
+
+    /**
+     * Set the original name (string representation of the key) for this space.
+     * This is used when a space is created via ConcurrentIndexCacheWrapperWithName.
+     *
+     * @param originalName The original name for this space
+     */
+    public void setOriginalName(String originalName) {
+        this.originalName = originalName;
+    }
+
+    /**
+     * Get the original name (string representation of the key) for this space.
+     * This may be null if the space was not created via ConcurrentIndexCacheWrapperWithName.
+     *
+     * @return The original name for this space, or null if not set
+     */
+    public String getOriginalName() {
+        return originalName;
     }
 
     @Override
@@ -46,6 +72,10 @@ public class BaseSpace<SelfT extends BaseSpace<SelfT> > implements Space {
     public static class BasicSpace extends BaseSpace<BasicSpace> implements Space {
         public BasicSpace(DriverAdapter<? extends CycleOp<?>, BasicSpace> adapter, long idx) {
             super(adapter, idx);
+        }
+
+        public BasicSpace(DriverAdapter<? extends CycleOp<?>, BasicSpace> adapter, long idx, String originalName) {
+            super(adapter, idx, originalName);
         }
     }
 }
