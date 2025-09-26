3f3f030bfb22940cf077461a9acea367b40cc5dc
Jonathan Shook <jshook@gmail.com>
2025-09-26 01:04:05 +0000

    license headers

--- nb-virtdata/virtdata-lib-vectors/src/main/java/io/nosqlbench/virtdata/lib/vectors/vectordata/CoreVectors.java
+++ nb-virtdata/virtdata-lib-vectors/src/main/java/io/nosqlbench/virtdata/lib/vectors/vectordata/CoreVectors.java
@@ -1,17 +1,30 @@
 package io.nosqlbench.virtdata.lib.vectors.vectordata;
 
+/*
+ * Copyright (c) nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 import io.nosqlbench.nbdatatools.api.concurrent.ProgressIndicatingFuture;
 import io.nosqlbench.vectordata.VectorTestData;
 import io.nosqlbench.vectordata.discovery.TestDataView;
 import io.nosqlbench.vectordata.downloader.Catalog;
 import io.nosqlbench.vectordata.downloader.DatasetEntry;
 import io.nosqlbench.vectordata.spec.datasets.types.DatasetView;
-import io.nosqlbench.virtdata.api.annotations.ThreadSafeMapper;
-
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.function.LongFunction;
 
 public abstract class CoreVectors<T> implements LongFunction<T> {
@@ -29,11 +42,7 @@ public abstract class CoreVectors<T> implements LongFunction<T> {
         if (prebuffer) {
             CompletableFuture<Void> pbfuture = dataset.prebuffer();
             if (pbfuture instanceof ProgressIndicatingFuture<Void> indicator) {
-//                try {
                     indicator.monitorProgress(1000);
-//                } catch (InterruptedException | ExecutionException | TimeoutException e) {
-//                    throw new RuntimeException(e);
-//                }
             }
         }
     }
